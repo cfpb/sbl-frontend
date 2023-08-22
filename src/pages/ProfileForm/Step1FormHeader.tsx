3384106241e02f3809705c4d5b1f708fb71e2222
shindigira <shindigira@gmail.com>
2023-08-22 12:27:20 -0400

    chore: added additional notice regarding LEI to the User Profile form

--- src/pages/ProfileForm/Step1FormHeader.tsx
+++ src/pages/ProfileForm/Step1FormHeader.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'design-stories';
+
 /**
  * 
  * @returns Header for Step1Form
@@ -8,7 +10,10 @@ function Step1FormHeader(): JSX.Element {
             {/* <h1>Complete your user profile</h1>
             <p className="lead-paragraph">In order to complete your user profile and access the filing platform your financial institution must have a Legal Entity Identifier (LEI). Visit the Global LEI Foundation (GLEIF) website for information on how to obtain an LEI for your institution.</p> */}
             <h1 className="text-[26px] md:text-[30px] font-semibold leading-tight mb-[0.44117647em]">Complete your user profile</h1>
-            <p className="text-[18px] md:text-[20px] font-normal leading-tight mb-8">In order to complete your user profile and access the filing platform your financial institution must have a Legal Entity Identifier (LEI). Visit the Global LEI Foundation (GLEIF) website for information on how to obtain an LEI for your institution.</p>
+            <p className="text-[18px] md:text-[20px] font-normal leading-tight mb-[30px]">In order to complete your user profile and access the filing platform your financial institution must have a Legal Entity Identifier (LEI). Visit the Global LEI Foundation (GLEIF) website for information on how to obtain an LEI for your institution.</p>
+            <p className="leading-paragraph mb-[30px]">
+              In order to begin using the filing platform you must have a Legal Entity identifier (LEI) for your financial institution. Visit the <Link href="#">Global LEI Foundation (GLEIF)</Link> website for more information on how to obtain an LEI.
+            </p>
             <p />
         </div>
   )
