6c94b459ed9b89c0c480ca90fb81ebf918342609
S T <shindigira@gmail.com>
2023-08-24 02:55:42 -0400

    Update src/pages/ProfileForm/Step1FormHeader.tsx
    Removed duplicate paragraph
    
    Co-authored-by: Meis <meissadia@gmail.com>

--- src/pages/ProfileForm/Step1FormHeader.tsx
+++ src/pages/ProfileForm/Step1FormHeader.tsx
@@ -7,8 +7,6 @@ import { Link } from 'design-system-react';
 function Step1FormHeader(): JSX.Element {
   return (
           <div className="max-w-[670px]">
-            {/* <h1>Complete your user profile</h1>
-            <p className="lead-paragraph">In order to complete your user profile and access the filing platform your financial institution must have a Legal Entity Identifier (LEI). Visit the Global LEI Foundation (GLEIF) website for information on how to obtain an LEI for your institution.</p> */}
             <h1 className="text-[26px] md:text-[30px] font-semibold leading-tight mb-[0.44117647em]">Complete your user profile</h1>
             <p className="text-[18px] md:text-[20px] font-normal leading-tight mb-[30px]">In order to complete your user profile and access the filing platform your financial institution must have a Legal Entity Identifier (LEI). Visit the Global LEI Foundation (GLEIF) website for information on how to obtain an LEI for your institution.</p>
             <p className="leading-paragraph mb-[30px]">
