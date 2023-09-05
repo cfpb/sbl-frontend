9ae55b900b6a9f6e315427d3e505d3698367ac6a
S T <shindigira@gmail.com>
2023-09-05 11:30:18 -0400

    Environment Variable Prefix changes (#47)
    * Environment Variable Prefix changes
    
    Changes:
    
    - [feat] Changed the envPrefix to SBL_
    
    * docs: comment
    
    * Removed .env
    
    * ignore .env

--- src/api/oidc.ts
+++ src/api/oidc.ts
@@ -5,7 +5,7 @@ export interface OidcConfig {
 }
 
 export const oidcConfig: OidcConfig = {
-  authority: import.meta.env.VITE_OIDC_AUTHORITY as string,
-  client_id: import.meta.env.VITE_OIDC_CLIENT_ID as string,
-  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI as string
+  authority: import.meta.env.SBL_OIDC_AUTHORITY as string,
+  client_id: import.meta.env.SBL_OIDC_CLIENT_ID as string,
+  redirect_uri: import.meta.env.SBL_OIDC_REDIRECT_URI as string
 };
