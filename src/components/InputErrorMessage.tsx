6a7f393581e04d39e781f772abe7b1a1ebcb3ea8
shindigira <shindigira@gmail.com>
2023-09-09 10:06:06 -0400

    fix: added bracket

--- src/components/InputErrorMessage.tsx
+++ src/components/InputErrorMessage.tsx
@@ -7,7 +7,7 @@ interface Properties {
 
 function InputErrorMessage({children}: Properties): JSX.Element {
   return (
-    <div className="a-form-alert a-form-alert__error mt-[0.9375em]" role="alert"><ErrorIcon /><p className="a-form-alert_text ml-6 -translate-y-[6%] text-[#1B1B1B">{children}</p></div>
+    <div className="a-form-alert a-form-alert__error mt-[0.9375em]" role="alert"><ErrorIcon /><p className="a-form-alert_text ml-6 -translate-y-[6%] text-[#1B1B1B]">{children}</p></div>
 
   )
 }
