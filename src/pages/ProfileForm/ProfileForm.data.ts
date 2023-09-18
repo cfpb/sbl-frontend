e17a7e4643533e4235faa13c444f389d7a992261
shindigira <shindigira@gmail.com>
2023-09-18 16:02:28 -0400

    chore: renamed financial institutions names

--- src/pages/ProfileForm/ProfileForm.data.ts
+++ src/pages/ProfileForm/ProfileForm.data.ts
@@ -2,19 +2,19 @@ import type { FiDataType } from 'pages/ProfileForm/types';
 
 const fiData: FiDataType[] = [
   {
-    name: "Suntrust Banks, Inc",
+    name: "Generic Financial Institution 1",
     lei: "7E1PDLW1JLaTSoBS1Go3",
     taxID: "39-0432357",
     agencyCode: 3
   },
   {
-    name: "JP Morgan, Inc",
+    name: "Generic Financial Institution 2",
     lei: "8E1ODLE1JLaSVoBS1Bo2",
     taxID: "58-0838387",
     agencyCode: 4
   },
     {
-    name: "Bank of America, Inc",
+    name: "Generic Financial Institution 3",
     lei: "3E89DLE1JBaLEoBS1Co1",
     taxID: "62-0531257",
     agencyCode: 4
