3a8e713885e3a81cb62fe603396abcc48302244b
shindigira <shindigira@gmail.com>
2023-09-26 16:34:23 -0400

    feat: Step2 draft complete

--- src/pages/ProfileForm/Step2Form/Step2Form.tsx
+++ src/pages/ProfileForm/Step2Form/Step2Form.tsx
@@ -1,11 +1,17 @@
 import useProfileForm from "store/useProfileForm";
 
+import { Link, Notification } from 'design-system-react';
 import Step2FormHeader from './Step2FormHeader';
 
 interface Properties {
 }
 
 function Step2Form({}: Properties): JSX.Element {
+  
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
   const profileData = useProfileForm((state) => state.profileData);
   
   console.log("profileData: ", profileData);
@@ -13,7 +19,35 @@ function Step2Form({}: Properties): JSX.Element {
   return (
     <div>
       <Step2FormHeader />
-      <h4>First Name</h4>
+      <h3 className='mb-[30px]'>User profile details</h3>
+      <div className='mb-[30px]'>
+        <h4 className="mb-2">First name</h4>
+        <p>{profileData.firstName}</p>
+      </div>
+      <div className='mb-[30px]'>
+        <h4 className="mb-2">Last name</h4>
+        <p>{profileData.lastName}</p>
+      </div>
+      <div className='mb-[30px]'>
+        <h4 className="mb-2">Email address</h4>
+        <p>{profileData.email}</p>
+      </div>
+      <div className='mb-[30px]'>
+        <h4 className="mb-2">Associated financial institutions</h4>
+        {profileData.financialInstitutions.map(obj => {
+          const success = Boolean(randomIntFromInterval(0, 1));
+          return (
+        <div className='mb-2 flex flex-row gap-[15px]'>
+          <div className="min-w-[6.25rem]">
+            <Notification isFieldLevel type={ success ? "success" : "warning"} message={ success ? "Approved" : "Pending"} />
+          </div>
+          <Link href={'#'}>{`${obj.name} | ${obj.lei}`}</Link>
+        </div>
+        )
+        })}
+      </div>
+      
+      
       
     </div>
   )
