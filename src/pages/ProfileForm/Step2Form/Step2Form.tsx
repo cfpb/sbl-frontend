9ffdac2e55faedc28e663f0ebf6014ecfe4b9bda
shindigira <shindigira@gmail.com>
2023-09-26 15:07:17 -0400

    chore: extra profile data on step2

--- src/pages/ProfileForm/Step2Form/Step2Form.tsx
+++ src/pages/ProfileForm/Step2Form/Step2Form.tsx
@@ -1,9 +1,15 @@
+import useProfileForm from "store/useProfileForm";
+
 import Step2FormHeader from './Step2FormHeader';
 
 interface Properties {
 }
 
 function Step2Form({}: Properties): JSX.Element {
+  const profileData = useProfileForm((state) => state.profileData);
+  
+  console.log("profileData: ", profileData);
+
   return (
     <div>
       <Step2FormHeader />
