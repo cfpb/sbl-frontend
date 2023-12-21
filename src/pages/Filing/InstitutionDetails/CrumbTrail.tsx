dbb3d853e251e7da4383152b2ae3e58113b0ddd6
Meis <meissadia@gmail.com>
2023-12-21 10:45:03 -0700

    fix: Address array key error for CrumbTrail

--- src/pages/Filing/InstitutionDetails/CrumbTrail.tsx
+++ src/pages/Filing/InstitutionDetails/CrumbTrail.tsx
@@ -7,18 +7,24 @@ interface CrumbTrailProperties {
   children: ReactNode | ReactNode[];
 }
 
+const length = 10;
+const separatorKeys = Array.from({ length }).map((_, index) => `key-${index}`);
+const INCREMENT_BY_ONE = 1;
+
 function CrumbTrail({ children }: CrumbTrailProperties): JSX.Element | null {
   let items: ReactNode[] = [];
 
   // eslint-disable-next-line unicorn/no-null
   if (!children) return null;
 
+  let current = 0;
   if (Children.count(children) > SINGLE_CHILD) {
     Children.forEach(children, child => {
-      items.push(<> / </>, child);
+      items.push(<span key={separatorKeys[current]}> / </span>, child);
+      current += INCREMENT_BY_ONE;
     });
   } else {
-    items = [<> / </>, children];
+    items = [<span key={separatorKeys[current]}> / </span>, children];
   }
 
   return <div className='mb-5 font-normal'>{items}</div>;
