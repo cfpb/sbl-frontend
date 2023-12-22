79f717bd4169929b664beb7aa86e081580860d4f
Meis <meissadia@gmail.com>
2023-12-21 17:21:52 -0700

    [MVP] Institution details (#90)
    * Update dependencies
    
    * fix: Update DSR component name - Well => WellContent
    
    * deps: Add classnames
    
    * Institution details v0.1 - static
    
    * Institution details v0.2 - static
    
    * fix: Husky precommit linting and formatting
    
    * fix: ESLint issues
    
    * fix: Test data formatting
    
    * fix: Mobile styles for Institution details > Affiliate information
    
    * fix: remove ChangeHistory (not in MVP)
    
    * fix: Update Institution Details intro section;
    
    * feat: Add crumb trail
    
    * fix: Remove faux Affiliate information from faux API call
    
    * fix: Language update
    
    * fix: Return null for empty CrumbTrail
    
    * feat: useAuth - Redirect user back to originating url after login
    
    * fix: Address array key error for CrumbTrail
    
    * feat: Fetch Institution details from API
    
    * fix: Update language
    
    * fix: Add TODOs for pending items
    
    * fix: Update language for AffiliateInformation section
    
    * fix: Handle updated institution domain format

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ import {
 const FilingApp = lazy(async () => import('pages/Filing/FilingApp'));
 const FilingHome = lazy(async () => import('pages/Filing/FilingHome'));
 const ProfileForm = lazy(async () => import('pages/ProfileForm'));
+const InstitutionDetails = lazy(
+  async () => import('pages/Filing/InstitutionDetails/'),
+);
 
 /**
  * Determine if the current provided URL (href) is the current page
@@ -24,8 +27,7 @@ const ProfileForm = lazy(async () => import('pages/ProfileForm'));
  */
 const deriveClassname = (href: string): string => {
   let cname = 'nav-item';
-  let pattern = `${href}`;
-  if (href === '/') pattern += '$';
+  const pattern = `${href}$`;
 
   const regex = new RegExp(pattern);
   if (regex.test(window.location.href)) {
@@ -53,6 +55,11 @@ function BasicLayout(): ReactElement {
     <NavItem key='home' href='/' label='HOME' />,
     <NavItem key='filing' href='/filing' label='FILING' />,
     <NavItem key='profile-form' href='/profile-form' label='PROFILE FORM' />,
+    <NavItem
+      key='institution-details'
+      href='/institution/TESTBANK123'
+      label='INSTITUTION DETAILS'
+    />,
   ];
 
   const auth = useSblAuth();
@@ -94,15 +101,15 @@ function BasicLayout(): ReactElement {
 interface ProtectedRouteProperties {
   isAuthenticated: boolean;
   children: ReactNode;
+  onLogin: () => Promise<void>;
 }
 
 function ProtectedRoute({
   isAuthenticated,
+  onLogin,
   children,
-}: ProtectedRouteProperties): ReactNode {
-  if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
-  }
+}: ProtectedRouteProperties): Promise<void> | ReactNode {
+  if (!isAuthenticated) return onLogin();
   return children;
 }
 
@@ -121,8 +128,16 @@ export default function App(): ReactElement {
             <Route
               path='/filing'
               element={
-                <ProtectedRoute isAuthenticated={auth.isAuthenticated}>
+                <ProtectedRoute {...auth}>
                   <FilingApp />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/institution/:lei'
+              element={
+                <ProtectedRoute {...auth}>
+                  <InstitutionDetails />
                 </ProtectedRoute>
               }
             />
---
+++ src/pages/Filing/InstitutionDetails/IdentifyingInformation.tsx
@@ -0,0 +1,44 @@
+import { Heading, WellContainer } from 'design-system-react';
+import { DisplayField } from './DisplayField';
+import type { InstitutionDetailsApiType } from './institutionDetails.type';
+
+export function IdentifyingInformation({
+  data,
+}: {
+  data: InstitutionDetailsApiType;
+}): JSX.Element {
+  return (
+    <>
+      <Heading type='3' className='u-mt45'>
+        Identifying information
+      </Heading>
+      <p>
+        If you represent a non-depository institution you may add or update the
+        information in the fields below. If you represent a depository
+        institution, please reach out to NIC to make changes.
+      </p>
+
+      <WellContainer className='u-mt30'>
+        <DisplayField
+          label='Federal Taxpayer Identification Number (TIN)'
+          value={data.tax_id}
+        />
+        <DisplayField
+          label='Research, Statistics, Supervision, and Discount Identification
+                (RSSD ID) number'
+          value={data.rssd_id}
+        />
+        <DisplayField
+          label='Federal prudential regulator'
+          value={data.primary_federal_regulator_id}
+        />
+        <DisplayField
+          label='Type of financial institution'
+          value={data.sbl_institution_type_id}
+        />
+      </WellContainer>
+    </>
+  );
+}
+
+export default IdentifyingInformation;
