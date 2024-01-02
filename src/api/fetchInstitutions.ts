74e1b4770cfe4ce4e211b019ba59e95b697b52cd
Bill Himmelsbach <whimmels@gmail.com>
2024-01-02 02:29:21 -0800

    feat(routing): add route logic for profile-form and salesforce

--- src/api/fetchInstitutions.ts
+++ src/api/fetchInstitutions.ts
@@ -1,14 +1,13 @@
 import type { SblAuthProperties } from 'api/useSblAuth';
-import { BASE_URL } from 'api/common';
 import type { InstitutionDetailsApiType } from 'pages/Filing/InstitutionDetails/institutionDetails.type';
 
 export const fetchInstitutions = async (
   auth: SblAuthProperties,
+  domain?: string,
 ): Promise<InstitutionDetailsApiType[]> => {
-  const response = await fetch(`/v1/institutions`, {
+  const response = await fetch(`/v1/institutions${domain ? `?domain=${domain}` : '' }`, {
     headers: { Authorization: `Bearer ${auth.user?.access_token}` },
   });
-
   return response.json() as Promise<InstitutionDetailsApiType[]>;
 };
 
