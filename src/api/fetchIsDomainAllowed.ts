d14f21cddfb116a1c8fb89a7aef9d8df5e3d00b9
Bill Himmelsbach <whimmels@gmail.com>
2024-01-02 20:28:56 -0800

    spike(headers): hardcode backend value, pull out proxy

--- src/api/fetchIsDomainAllowed.ts
+++ src/api/fetchIsDomainAllowed.ts
@@ -1,10 +1,11 @@
 import type { SblAuthProperties } from 'api/useSblAuth';
+import { BASE_URL } from './common';
 
 export const fetchIsDomainAllowed = async (
   auth: SblAuthProperties,
   domain?: string,
 ): Promise<boolean> => {
-  const response = await fetch(`/v1/institutions/domains/allowed?domain=${domain}`, {
+  const response = await fetch(`${BASE_URL}/v1/institutions/domains/allowed?domain=${domain}`, {
     headers: { Authorization: `Bearer ${auth.user?.access_token}` },
   });
   return response.json() as Promise<boolean>;
