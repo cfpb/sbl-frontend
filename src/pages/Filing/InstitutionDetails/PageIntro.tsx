f3ab9d970170ba5cb3f67f81b48b94458e6e0fe8
Meis <meissadia@gmail.com>
2024-01-05 14:20:04 -0700

    fix: [Financial profile] Pull in latest updates from Figma

--- src/pages/Filing/InstitutionDetails/PageIntro.tsx
+++ src/pages/Filing/InstitutionDetails/PageIntro.tsx
@@ -1,22 +1,19 @@
-import { Link, TextIntroduction } from 'design-system-react';
+import Links from 'components/CommonLinks';
+import { TextIntroduction } from 'design-system-react';
 
 export function PageIntro(): JSX.Element {
   return (
     <TextIntroduction
-      heading='View financial institution profile'
+      heading='View your financial institution profile'
       subheading='The following reflects the most current information available to the CFPB for your financial institution. We pull data from sources such as GLEIF (Global Legal Entity Identifier Foundation), the National Information Center (NIC), and direct requests to our support staff.'
       description={
         <>
-          Requested changes to your financial institution details are handled by
-          our support staff. Requests take approximately 24-48 hours to be
-          processed.
+          Most changes to financial institution profile details must be handled
+          at the source (GLEIF and NIC). For all other changes please submit the
+          following form to our support staff.
         </>
       }
-      callToAction={
-        // TODO: replace this generic SBL Help link with a specific Salesforce form link, see:
-        // https://github.com/cfpb/sbl-frontend/issues/109
-        <Link href='https://sblhelp.consumerfinance.gov/'>Update your financial institution profile</Link>
-      }
+      callToAction={<Links.UpdateInstitutionProfile isCallToAction />}
     />
   );
 }
