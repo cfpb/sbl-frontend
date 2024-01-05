7aadbcca758a089fbde0bb565e64d42c41375d97
shindigira <shindigira@gmail.com>
2024-01-05 15:47:39 -0500

    refactor: Utilized DSR AlertFieldLevel in NoDatabaseResultError

--- src/pages/ProfileForm/Step1Form/NoDatabaseResultError.tsx
+++ src/pages/ProfileForm/Step1Form/NoDatabaseResultError.tsx
@@ -1,19 +1,18 @@
 import ErrorIcon from 'components/ErrorIcon';
-import { Link } from 'design-system-react';
+import { Link, AlertFieldLevel } from 'design-system-react';
+import { sblHelpLink } from 'utils/common';
 
 function NoDatabaseResultError(): JSX.Element {
   return (
     <div className='mt-[0.9375em] flex flex-row gap-2'>
-      <ErrorIcon />
-      <div className=''>
-        <p className='-translate-y-[3%]'>
-          The financial institution/LEI you searched for was not found in our
+      <AlertFieldLevel
+    message={<>The financial institution/LEI you searched for was not found in our
           database. If you recently registered for an LEI with GLEIF, your
           registration may still be in process. If you need further assistance
-          please <Link href='#'>submit a technical question</Link> to our help
-          desk.
-        </p>
-      </div>
+          please <Link href={sblHelpLink}>submit a technical question</Link> to our help
+          desk.</>}
+    status="error"
+  />
     </div>
   );
 }
