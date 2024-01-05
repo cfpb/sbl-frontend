f708e8fbc24e09f7daa3e5fbeac2e35bf74329c9
shindigira <shindigira@gmail.com>
2024-01-05 16:47:56 -0500

    refactor: auto-format

--- src/pages/ProfileForm/ProfileFormUtils.ts
+++ src/pages/ProfileForm/ProfileFormUtils.ts
@@ -1,8 +1,9 @@
 import type { ValidationSchema, FormattedUserProfileObjectType } from './types';
+
 export const formatUserProfileObject = (
   userProfileObject: ValidationSchema,
 ): FormattedUserProfileObjectType => ({
   first_name: userProfileObject.firstName,
   last_name: userProfileObject.lastName,
-  leis: userProfileObject.financialInstitutions.map(obj => obj.lei),
+  leis: userProfileObject.financialInstitutions.map(object => object.lei),
 });
