877d63ee55a522318f25551f06437216b44d970e
Meis <meissadia@gmail.com>
2024-01-04 17:25:41 -0700

    fix: [ReviewInstitutions] Use DSR components for Heading and Paragraph

--- src/pages/AuthenticatedLanding/ReviewInstitutions.tsx
+++ src/pages/AuthenticatedLanding/ReviewInstitutions.tsx
@@ -1,7 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import fetchAssociatedInstitutions from 'api/fetchAssociatedInstitutions';
 import useSblAuth from 'api/useSblAuth';
-import { Alert, Icon, Link, List, ListItem } from 'design-system-react';
+import {
+  Alert,
+  Heading,
+  Icon,
+  Link,
+  List,
+  ListItem,
+  Paragraph,
+} from 'design-system-react';
 import { SubsectionWrapper } from './SubsectionWrapper';
 
 export function ReviewInstitutions(): JSX.Element {
@@ -33,14 +41,16 @@ export function ReviewInstitutions(): JSX.Element {
   }
   return (
     <SubsectionWrapper>
-      <h3 className='heading'>View your associated financial institutions</h3>
-      <p>
+      <Heading type='3' className='heading'>
+        View your associated financial institutions
+      </Heading>
+      <Paragraph>
         You have successfully associated the following financial institutions
         with your user profile. If you have requested to add additional
         financial institution associations, you will not be able to file for
         them until they are approved by the support staff. Once they are
         approved they will show up below.
-      </p>
+      </Paragraph>
       <List isLinks className='institution-list'>
         {institutionList}
       </List>
