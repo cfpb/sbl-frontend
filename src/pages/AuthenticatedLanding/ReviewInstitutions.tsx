01e6c6e7414e5d7708d370f048977db4792f466c
Meis <meissadia@gmail.com>
2024-01-04 17:02:22 -0700

    fix: [ReviewInstitutions] Add loading state for Institution list

--- src/pages/AuthenticatedLanding/ReviewInstitutions.tsx
+++ src/pages/AuthenticatedLanding/ReviewInstitutions.tsx
@@ -1,37 +1,48 @@
 import { useQuery } from '@tanstack/react-query';
 import fetchAssociatedInstitutions from 'api/fetchAssociatedInstitutions';
 import useSblAuth from 'api/useSblAuth';
-import { Icon, Link, List, ListItem } from 'design-system-react';
+import { Alert, Icon, Link, List, ListItem } from 'design-system-react';
 import { SubsectionWrapper } from './SubsectionWrapper';
 
 export function ReviewInstitutions(): JSX.Element {
   const auth = useSblAuth();
   const email = auth.user?.profile.email;
   const { data: associatedInstitutions } = useQuery({
-    queryKey:  [`fetch-associated-institutions-${email}`, email],
+    queryKey: [`fetch-associated-institutions-${email}`, email],
     queryFn: async () => fetchAssociatedInstitutions(auth),
   });
+
+  let institutionList = [
+    <Alert
+      key='loading-associations'
+      status='loading'
+      message='Loading associated institutions...'
+    />,
+  ];
+
+  if (associatedInstitutions?.length) {
+    institutionList = associatedInstitutions.map(object => (
+      <ListItem key={object.lei}>
+        <Icon name='approved' withBg className='green' />
+        <span className='status-label'>Approved</span>
+        <Link href={`/institution/${object.lei}`} type='list'>
+          {object.name} | {object.lei}
+        </Link>
+      </ListItem>
+    ));
+  }
   return (
     <SubsectionWrapper>
-      <h3 className='heading'>Review financial institution details</h3>
+      <h3 className='heading'>View your associated financial institutions</h3>
       <p>
-        You are able to file data for the financial institutions that have been
-        verified. Pending selections will be reviewed by our technical help
-        team. You will not be able to file for those institutions until the
-        associations are approved. Please allow 24-48 hours for response time
-        during normal business hours.
+        You have successfully associated the following financial institutions
+        with your user profile. If you have requested to add additional
+        financial institution associations, you will not be able to file for
+        them until they are approved by the support staff. Once they are
+        approved they will show up below.
       </p>
       <List isLinks className='institution-list'>
-        {associatedInstitutions?.map(object => (
-            <ListItem key={object.lei}>
-                <Icon name='approved' withBg className='green' />
-                <span className='status-label'>Approved</span>
-                {/* TODO - Link to the "View Institution details" page */}
-                <Link href={`/institution/${object.lei}`} type='list'>
-                  {object.name} | {object.lei}
-                </Link>
-              </ListItem>
-          ))}
+        {institutionList}
       </List>
     </SubsectionWrapper>
   );
