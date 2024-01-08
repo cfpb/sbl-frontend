9f5d69524b6fac32007f970289d0b91998a7e737
Meis <meissadia@gmail.com>
2024-01-08 11:37:54 -0700

    fix: [Authenticated landing] Address Design team feedback

--- src/pages/AuthenticatedLanding/FileSbl.tsx
+++ src/pages/AuthenticatedLanding/FileSbl.tsx
@@ -1,4 +1,4 @@
-import { Link, List, ListItem } from 'design-system-react';
+import { List, ListLink } from 'design-system-react';
 import { SubsectionWrapper } from './SubsectionWrapper';
 
 export function FileSbl(): JSX.Element {
@@ -6,17 +6,16 @@ export function FileSbl(): JSX.Element {
     <SubsectionWrapper>
       <h2 className='heading'>File your small business lending data</h2>
       <p>
-        Covered financial institutions and voluntary reporters are required to maintain, report, and 
-        publicly disclose information about small business lending. These data are intended to help 
-        show whether lenders are serving the credit needs of small businesses in their communities, 
-        by increasing transparency in the lending marketplace.
+        Covered financial institutions and voluntary reporters are required to
+        maintain, report, and publicly disclose information about small business
+        lending. This data is intended to help show whether lenders are serving
+        the credit needs of small businesses in their communities, by increasing
+        transparency in the lending marketplace.
       </p>
-      <List isLinks>
-        <ListItem>
-          <Link href={`/filing?${Date.now().toString()}`}>
-            File your small business lending data
-          </Link>
-        </ListItem>
+      <List isLinks className='mt-[15px]'>
+        <ListLink href={`/filing?${Date.now().toString()}`}>
+          File your small business lending data
+        </ListLink>
       </List>
     </SubsectionWrapper>
   );
--- src/pages/AuthenticatedLanding/index.tsx
+++ src/pages/AuthenticatedLanding/index.tsx
@@ -1,7 +1,7 @@
 import './Landing.less';
 
 import AdditionalResources from 'components/AdditionalResources';
-import { Divider, Hero, Layout, Link, ListItem } from 'design-system-react';
+import { Divider, Hero, Layout, ListLink } from 'design-system-react';
 import type { ReactElement } from 'react';
 import { FileSbl } from './FileSbl';
 import { ReviewInstitutions } from './ReviewInstitutions';
@@ -17,38 +17,32 @@ function Landing(): ReactElement {
         />
         <Layout.Wrapper>
           <Layout.Content id='main-content'>
-            {/* <FileHMDA />
-            <Divider/> */}
+            {/* 
+            TODO: Include link to Filing app once it's built
+            <FileHMDA />
+            <Divider/> 
+            */}
             <FileSbl />
             <Divider />
             <ReviewInstitutions />
           </Layout.Content>
           <Layout.Sidebar id='sidebar'>
             <AdditionalResources>
-              <ListItem>
-                <Link 
-                  href="https://www.consumerfinance.gov/rules-policy/final-rules/small-business-lending-under-the-equal-credit-opportunity-act-regulation-b/"
-                >
-                  Final Rule
-                </Link>          
-              </ListItem>
-              <ListItem>
-                <Link
-                  href="https://www.consumerfinance.gov/data-research/small-business-lending/filing-instructions-guide/2024-guide/"
-                >
-                  Filing instructions guide
-                </Link>              
-              </ListItem>
-              <ListItem>
-                <Link 
-                  href="https://www.consumerfinance.gov/compliance/compliance-resources/small-business-lending-resources/small-business-lending-collection-and-reporting-requirements/"
-                >
-                  Collection and reporting requirements
-                </Link>
-              </ListItem>
+              <ListLink href='https://www.consumerfinance.gov/rules-policy/final-rules/small-business-lending-under-the-equal-credit-opportunity-act-regulation-b/'>
+                Final Rule
+              </ListLink>
+              <ListLink href='https://www.consumerfinance.gov/data-research/small-business-lending/filing-instructions-guide/2024-guide/'>
+                Filing instructions guide
+              </ListLink>
+              <ListLink href='https://www.consumerfinance.gov/compliance/compliance-resources/small-business-lending-resources/small-business-lending-collection-and-reporting-requirements/'>
+                Collection and reporting requirements
+              </ListLink>
             </AdditionalResources>
-            {/* <Divider/> */}
-            {/* <MailingListSignup     /> */}
+            {/* 
+            TODO: Include Mailing List signup post-MVP
+            <Divider />
+            <MailingListSignup /> 
+            */}
           </Layout.Sidebar>
         </Layout.Wrapper>
       </Layout.Main>
