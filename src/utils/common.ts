fac13337a146018fd322f073a7f2ec68c55ade83
shindigira <shindigira@gmail.com>
2024-01-09 11:04:06 -0500

    chore: added TODO

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,10 @@
 export const sblHelpLink = 'https://sblhelp.consumerfinance.gov/';
+// TODO: replace this generic SBL Help link with a specific Salesforce form links
+// depending on the page and the form it should route to, see:
+// https://github.com/cfpb/sbl-frontend/issues/109
 export const gleifLink = 'https://www.gleif.org/';
 
 export default {
   sblHelpLink,
-  gleifLink
-}
+  gleifLink,
+};
