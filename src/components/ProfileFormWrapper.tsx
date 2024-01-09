1de39328c8252c35d1ebe268ec4eabaf2d90f49d
shindigira <shindigira@gmail.com>
2024-01-09 16:47:52 -0500

    fix: resolved merge conflicts

--- src/components/ProfileFormWrapper.tsx
+++ src/components/ProfileFormWrapper.tsx
@@ -6,9 +6,9 @@ interface Properties {
 
 function ProfileFormWrapper({children}: Properties): JSX.Element {
   return (
-    <div className="ml-5 mr-5 mt-[45px]">
-      <div className="max-w-[1200px] mx-auto mb-12">
-        <div className="max-w-[770px] mx-auto">
+    <div className="ml-5 mr-5 mt-[2.813rem]">
+      <div className="max-w-[75rem] mx-auto mb-12">
+        <div className="max-w-[48.125rem] mx-auto">
           { children }
         </div>
       </div>
