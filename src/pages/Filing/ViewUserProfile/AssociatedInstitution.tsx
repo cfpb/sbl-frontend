658acb14b0e0cc6e15f0c42566abdc4f861da38b
Meis <meissadia@gmail.com>
2024-01-10 17:32:49 -0700

    fix: [AssociatedInstitution] - Split AssociatedInstitution into reusable component - Update heading to H2 - Prettier formatting

---
+++ src/pages/Filing/ViewUserProfile/AssociatedInstitution.tsx
@@ -0,0 +1,35 @@
+import { Icon, Link, ListItem } from 'design-system-react';
+import type { InstitutionDetailsApiType } from 'pages/Filing/InstitutionDetails/institutionDetails.type';
+
+export function AssociatedInstitution({
+  name,
+  lei,
+}: InstitutionDetailsApiType): JSX.Element {
+  let displayText = null;
+
+  // Generate the Institution's label
+  if (name && lei) displayText = `${name} | ${lei}`;
+  else if (!name && !lei)
+    displayText = 'Missing Institution details; please contact SBL Help';
+  else if (!lei) displayText = `"${name}" has no LEI; please contact SBL Help`;
+  else if (!name) displayText = lei;
+
+  // Do we have enough info to link to the Institution's details page?
+  const institutionLabel = lei ? (
+    <Link href={`/institution/${lei}`} type='list'>
+      {displayText}
+    </Link>
+  ) : (
+    <span>{displayText}</span>
+  );
+
+  return (
+    <ListItem key={lei}>
+      <Icon name='approved' withBg className='green' />
+      <span className='status-label'>Approved</span>
+      {institutionLabel}
+    </ListItem>
+  );
+}
+
+export default AssociatedInstitution;
