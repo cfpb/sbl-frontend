ebc0b5b1420548e974a8e3603ca62663271a8043
Bill Himmelsbach <whimmels@gmail.com>
2024-01-11 08:11:10 -0800

    fix(home): baseline for steps, new heading designs

--- src/components/AdditionalResources.tsx
+++ src/components/AdditionalResources.tsx
@@ -11,7 +11,7 @@ export function AdditionalResources({
 }: SubsectionWrapperProperties): JSX.Element {
   return (
     <div className='additional-resources' {...properties}>
-      <Heading type='5'>Additional resources</Heading>
+      <Heading type='4'>Additional resources</Heading>
       <List className='list-none pl-0' isLinks>
         {children}
       </List>
