9594185c9a47d42dd4037e485ef21a7bea530bf9
Meis <meissadia@gmail.com>
2024-01-11 12:51:48 -0700

    fix: [User profile] remove top margin

--- src/pages/Filing/ViewUserProfile/index.tsx
+++ src/pages/Filing/ViewUserProfile/index.tsx
@@ -44,7 +44,7 @@ export default function ViewUserProfile(): JSX.Element {
     <Grid.Wrapper center>
       <Grid.Row>
         <Grid.Column width={8}>
-          <main id='main-content' className='my-10'>
+          <main id='main-content'>
             <CrumbTrail>
               <a href='/landing' key='home'>
                 Platform home
