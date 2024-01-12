d1e801ab21a0ca40d43ce21bc68b8f95395fedba
Meis <meissadia@gmail.com>
2024-01-11 17:06:58 -0700

    fix: [404] Update content, theme, spacing

--- src/pages/Error/NotFound404.tsx
+++ src/pages/Error/NotFound404.tsx
@@ -19,33 +19,21 @@ export function NotFound404(): ReactElement {
               </header>
             </div>
 
-            <div className='error_action_container'>
-              <div className='error_action_col'>
-                <Link href='/' className='a-btn a-btn__full-on-xs'>
-                  Platform homepage
-                </Link>
-              </div>
-              <div className='error_action_col'>
-                <Link
-                  className='a-btn a-btn__full-on-xs'
-                  href='/about-us/contact-us/'
-                >
-                  Contact us
-                </Link>
-              </div>
-            </div>
-
-            <Paragraph>
-              Are you sure this is the right web address?&nbsp;
-              <Link
-                className='a-link a-link__jump'
-                href={`mailto:CFPB_website@consumerfinance.gov?subject=404%20Error%20at%20%22${encodeURIComponent(
-                  window.location.href,
-                )}%22`}
-              >
-                <span className='a-link_text'>Let us know</span>
+            <aside className='error_action_container'>
+              <Link href='/' className='a-btn a-btn__full-on-xs'>
+                Visit platform homepage
               </Link>
-            </Paragraph>
+
+              <Paragraph className='contact-us'>
+                Are you sure this is the right web address?&nbsp;
+                <Link
+                  className='a-link a-link__jump'
+                  href='https://sblhelp.consumerfinance.gov/'
+                >
+                  <span className='a-link_text'>Contact our support staff</span>
+                </Link>
+              </Paragraph>
+            </aside>
           </section>
         </div>
       </div>
