f60e46b7944deb4ffc5e98be9c9a9256f24cc7f0
shindigira <shindigira@gmail.com>
2024-01-19 02:56:30 -0500

    style: added href to allow cursor:pointer on error links

--- src/pages/ProfileForm/Step1Form/Step1FormErrorHeader.tsx
+++ src/pages/ProfileForm/Step1Form/Step1FormErrorHeader.tsx
@@ -1,4 +1,4 @@
-import { Alert, List } from 'design-system-react';
+import { Alert, List, ListItem } from 'design-system-react';
 import { Link } from 'react-scroll';
 
 import { FormFieldsHeaderError as formFieldsHeaderError } from 'pages/ProfileForm/types';
@@ -43,27 +43,25 @@ function Step1FormErrorHeader({
           };
 
           return (
-            <span key={key}>
-              {/* Uses the ListLink styling */}
-              <li className='m-list_item'>
-                <Link
-                  className='m-list_link'
-                  to={key}
-                  smooth
-                  duration={300}
-                  offset={-100}
-                  onClick={onHandleFocus}
-                  onKeyPress={onHandleKeyPress}
-                  tabIndex={0}
-                >
-                  {
-                    formFieldsHeaderError[
-                      key as keyof typeof formFieldsHeaderError
-                    ]
-                  }
-                </Link>
-              </li>
-            </span>
+            <ListItem key={key}>
+              <Link
+                href='#'
+                className='m-list_link'
+                to={key}
+                smooth
+                duration={300}
+                offset={-100}
+                onClick={onHandleFocus}
+                onKeyPress={onHandleKeyPress}
+                tabIndex={0}
+              >
+                {
+                  formFieldsHeaderError[
+                    key as keyof typeof formFieldsHeaderError
+                  ]
+                }
+              </Link>
+            </ListItem>
           );
         })}
       </List>
