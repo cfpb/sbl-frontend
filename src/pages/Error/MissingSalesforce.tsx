1e3b02b67ffaeac67ec238eedb3b7aee1f6a8c9d
Bill Himmelsbach <whimmels@gmail.com>
2024-01-31 08:02:59 -0800

    fix(missingsalesforce): incorporate design feedback

--- src/pages/Error/MissingSalesforce.tsx
+++ src/pages/Error/MissingSalesforce.tsx
@@ -2,7 +2,7 @@
 // Remove Salesforce placeholder after POC, see: https://github.com/cfpb/sbl-frontend/issues/214
 
 import { Link } from 'components/Link';
-import { Button, Heading, Paragraph } from 'design-system-react';
+import { ListItem, TextIntroduction } from 'design-system-react';
 import type { ReactElement } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { One } from 'utils/constants';
@@ -24,51 +24,50 @@ export function MissingSalesforce(): ReactElement {
     <main className='content error_main error_main__404' id='main'>
       <div className='content_wrapper'>
         <div
-          className='content_main '
+          className='content_main !mb-0'
           style={{
-            background: `transparent url('/static/salesforce-logo.svg') no-repeat right bottom`,
+            background: `transparent url('/static/salesforce-logo.svg') no-repeat right center`,
             backgroundSize: '35%',
           }}
         >
           <section className='content_main-inner'>
             <div className='error_copy_container'>
               <header className='error_header'>
-                <Heading>
-                  We&apos;re currently building our own Salesforce forms
-                </Heading>
-                <Paragraph isLead>
-                  We have recently been given the opportunity to make our own
-                  help forms that connect to the Salesforce platform, which we
-                  believe will create a smoother user experience that also
-                  better fits within our deadlines. We are planning to have a
-                  proof of concept completed by the next sprint review, February
-                  14th.
-                </Paragraph>
+                <TextIntroduction
+                  heading='We are currently building our own Salesforce forms'
+                  subheading='We have recently been given the opportunity to make our own help forms that connect to the Salesforce platform, which we believe will create a smoother user experience that also better fits within our deadlines.'
+                  callToAction={
+                    <>
+                      <ListItem>
+                        <Link
+                          href={
+                            isCompleteUserProfile
+                              ? completeYourUserProfileImg
+                              : updateYourFinancialInstitutionImg
+                          }
+                          isRouterLink={false}
+                          type='list'
+                        >
+                          {isCompleteUserProfile
+                            ? 'View complete your user profile design'
+                            : 'View update your financial institution profile design'}
+                        </Link>
+                      </ListItem>
+                      <ListItem>
+                        <Link
+                          onClick={onBackButtonClick}
+                          href='/'
+                          label='Go back to previous page'
+                          type='list'
+                        >
+                          Go back to previous page
+                        </Link>
+                      </ListItem>
+                    </>
+                  }
+                />
               </header>
             </div>
-
-            <aside className='error_action_container'>
-              <Link
-                className='a-btn a-btn__full-on-xs !mr-10'
-                href={
-                  isCompleteUserProfile
-                    ? completeYourUserProfileImg
-                    : updateYourFinancialInstitutionImg
-                }
-                isRouterLink={false}
-              >
-                {isCompleteUserProfile
-                  ? 'View complete your user profile design'
-                  : 'View update your financial institution profile design'}
-              </Link>
-              <Button
-                onClick={onBackButtonClick}
-                className='a-btn a-btn__full-on-xs'
-                label='Go back to previous page'
-              >
-                Go back to previous page
-              </Button>
-            </aside>
           </section>
         </div>
       </div>
