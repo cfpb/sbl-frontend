a7b7c2dd7515a8d2ffde163e833f64b760967b01
shindigira <shindigira@gmail.com>
2024-02-09 14:31:52 -0500

    feat: Step1FormInfoFieldGroup

---
+++ src/pages/ProfileForm/Step1Form/Step1FormInfoFieldGroup.tsx
@@ -0,0 +1,52 @@
+import useSblAuth from 'api/useSblAuth';
+import FieldGroup from 'components/FieldGroup';
+import InputEntry from 'components/InputEntry';
+import { Paragraph } from 'design-system-react';
+import type { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface Step1FormInfoFieldGroupProperties {
+  register: UseFormRegister<FieldValues>;
+  formErrors: object;
+}
+
+function Step1FormInfoFieldGroup({
+  register,
+  formErrors,
+}: Step1FormInfoFieldGroupProperties): JSX.Element {
+  const { emailAddress } = useSblAuth();
+  return (
+    <div className='mb-[3.75rem]'>
+      <FieldGroup>
+        <div className='mb-[1.875rem]'>
+          <InputEntry
+            label='First name'
+            id='firstName'
+            {...register('firstName')}
+            errors={formErrors}
+            isDisabled={false}
+          />
+          <InputEntry
+            label='Last name'
+            id='lastName'
+            {...register('lastName')}
+            errors={formErrors}
+            isDisabled={false}
+          />
+        </div>
+        <InputEntry
+          label='Email address'
+          id='email'
+          {...register('email')}
+          errors={formErrors}
+          isDisabled
+          isLast
+          hideInput
+        >
+          <Paragraph className='mb-0'>{emailAddress}</Paragraph>
+        </InputEntry>
+      </FieldGroup>
+    </div>
+  );
+}
+
+export default Step1FormInfoFieldGroup;
