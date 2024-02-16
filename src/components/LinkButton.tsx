043f0c5cac945dbc2b4fb7c4a136a89e973f05b5
shindigira <shindigira@gmail.com>
2024-02-16 13:20:24 -0500

    refactor: LinkButton uses a button instead of anchor

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,28 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Icon, Link, LinkText } from 'design-system-react';
+import { Button, Icon, LinkText } from 'design-system-react';
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 interface LinkButtonProperties {
   icon: string;
   children: ReactNode;
-  onClick: React.ChangeEvent;
 }
 
 function LinkButton({
   className,
   icon = 'plus',
-  onClick,
   children,
-}: ComponentPropsWithoutRef<'a'> & LinkButtonProperties): JSX.Element {
+  ...other
+}: ComponentPropsWithoutRef<'button'> & LinkButtonProperties): JSX.Element {
   return (
-    <Link
+    <Button
       className={`cursor-pointer ${className}`}
-      onClick={onClick}
-      isJumpLeft
-    >
-      <Icon name={icon} />
-      <LinkText className='ml-2'>{children}</LinkText>
-    </Link>
+      // TODO: https://github.com/cfpb/design-system-react/issues/311
+      // @ts-expect-error label should be ReactNode, fix should be in DSR Repo
+      label={
+        <>
+          <Icon name={icon} />
+          <LinkText className='ml-2'>{children}</LinkText>
+        </>
+      }
+      asLink
+      {...other}
+    />
   );
 }
 
