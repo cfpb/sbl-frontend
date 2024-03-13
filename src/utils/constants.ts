9ba4c2a504570e8ccfc717cad46e0549adbe1d1f
Meis <meissadia@gmail.com>
2024-03-13 12:37:42 -0600

    Remove unused constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,4 @@
 // Needed due to ESLint rule: @typescript-eslint/no-magic-numbers
 
 export const Zero = 0;
-export const One = 1;
-export const Two = 2;
-export const Three = 3;
-export const Four = 4;
 export const Five = 5;
