3af84aeb7987c1a9b52488e7bf77d9be06824c1d
shindigira <shindigira@gmail.com>
2024-03-19 22:20:41 -1000

    feat(uploader): skeleton start

--- src/pages/Filing/FilingApp/FileSubmission.tsx
+++ src/pages/Filing/FilingApp/FileSubmission.tsx
@@ -1,6 +1,9 @@
 import CrumbTrail from 'components/CrumbTrail';
+import FieldGroup from 'components/FieldGroup';
+import FileInput from 'components/FileInput';
+import FormMain from 'components/FormMain';
 import Head from 'components/Head';
-import { Button, Grid, Heading, Icon, Link } from 'design-system-react';
+import { Grid, Label, Link } from 'design-system-react';
 import { useParams } from 'react-router-dom';
 
 const StepBasis = 'grow border-x-0 border-b-0 border-t-4 border-solid';
@@ -9,6 +12,20 @@ const StepPending = `${StepBasis} border-slate-500`;
 
 export function FileSubmission(): JSX.Element {
   const { lei } = useParams();
+
+  const onHandleFileChange = event => {
+    const file = event.target.files[0];
+
+    console.log('file:', file);
+    // const reader = new FileReader();
+
+    // reader.onload = (e) => {
+    //   const content = e.target.result;
+    //   setCsvData(content);
+    // };
+
+    // reader.readAsText(file);
+  };
 
   return (
     <>
@@ -31,14 +48,30 @@ export function FileSubmission(): JSX.Element {
               <div className={StepPending}>Step4</div>
               <div className={StepPending}>Step5</div>
             </div>
-            <Heading className='my-10'>Upload file for {lei}</Heading>
+            {/* <Heading className='my-10'>Upload file for {lei}</Heading>
             <div className='align-center flex w-full flex-nowrap content-center bg-slate-200 py-20  text-2xl'>
               <div className='w-full content-center justify-center pt-5 text-center text-slate-600'>
                 <Icon name='upload' className='mr-3' /> Upload File
               </div>
-            </div>
+            </div> */}
             <div className='my-10'>
-              <Button label='Next step' iconRight='arrow-right' />
+              {/* <Button label='Next step' iconRight='arrow-right' /> */}
+              <FormMain>
+                <FieldGroup>
+                  <Label htmlFor='file-input-specific'>Upload your files</Label>
+                  <div id='file-input-specific-hint'>
+                    Select one or more CSV files
+                  </div>
+                  <FileInput
+                    id='file-input-specific'
+                    name='file-input-specific'
+                    accept='.csv'
+                    aria-describedby='file-input-specific-hint'
+                    multiple
+                    onChange={onHandleFileChange}
+                  />
+                </FieldGroup>
+              </FormMain>
             </div>
           </Grid.Column>
         </Grid.Row>
