8d6887ee5148b10510d54957e75b888b90671f2c
Meis <meissadia@gmail.com>
2024-03-26 15:57:43 -0600

    [UpdateFinancialProfile] 60px margin between page footer and form content

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -7,7 +7,7 @@ interface FormWrapperProperties {
 function FormWrapper({ children }: FormWrapperProperties): JSX.Element {
   return (
     <div className='ml-5 mr-5 mt-[2.813rem]'>
-      <div className='mx-auto mb-12 max-w-[75rem]'>
+      <div className='mx-auto mb-[3.75rem] max-w-[75rem]'>
         <div className='mx-auto max-w-[48.125rem]'>{children}</div>
       </div>
     </div>
