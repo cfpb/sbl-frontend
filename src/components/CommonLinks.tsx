2aca3c0e518d869eb5af510f7f5acf6e0ff68b75
Meis <meissadia@gmail.com>
2024-03-28 10:37:12 -0600

    [Fix] Correct URL when linking to UFP form

--- src/components/CommonLinks.tsx
+++ src/components/CommonLinks.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'components/Link';
 import type { ReactElement } from 'react';
+import { useParams } from 'react-router-dom';
 
 function GLIEF(): ReactElement {
   return <Link href='https://www.gleif.org/'>GLEIF</Link>;
@@ -16,8 +17,9 @@ interface UpdateInstitutionProfileProperties {
 function UpdateInstitutionProfile({
   isCallToAction,
 }: UpdateInstitutionProfileProperties): ReactElement {
+  const { lei } = useParams();
   return (
-    <Link href='/update-financial-profile'>
+    <Link href={`/institution/${lei}/update`}>
       {isCallToAction
         ? 'Update your financial institution profile'
         : 'update financial institution profile'}
