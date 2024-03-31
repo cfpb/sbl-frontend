4ab3fdac421909b661e7ff82314ffea1fa451230
shindigira <shindigira@gmail.com>
2024-03-31 05:34:53 -0400

    feat(InlineStatus): use of Icon's accessbility - aria-labelledBy

--- src/components/InlineStatus.tsx
+++ src/components/InlineStatus.tsx
@@ -1,5 +1,6 @@
 import { Icon } from 'design-system-react';
 import type { ReactNode } from 'react';
+import { useId } from 'react';
 
 interface InlineStatusProperties {
   status: string;
@@ -13,16 +14,20 @@ function InlineStatus({
   message = '',
   ...other
 }: InlineStatusProperties): JSX.Element {
+  const iconId = useId();
   return (
     <div>
       <Icon
+        ariaLabelledby={iconId}
         name={status || 'updating'}
         className={`${
           status ? '' : 'updating invisible'
         } mr-[0.3125rem] inline-block ${className}`}
         {...other}
       />
-      <span className='inline-block'>{message}</span>
+      <span id={iconId} className='inline-block'>
+        {message}
+      </span>
     </div>
   );
 }
