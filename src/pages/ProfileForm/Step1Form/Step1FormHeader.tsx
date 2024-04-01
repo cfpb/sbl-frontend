ea9d6be7cc93e32ec19da9a67929c91695fc68ee
shindigira <shindigira@gmail.com>
2024-04-01 16:42:05 -0400

    content(Step1FormHeader): noAssoc withAssoc subheading change

--- src/pages/ProfileForm/Step1Form/Step1FormHeader.tsx
+++ src/pages/ProfileForm/Step1Form/Step1FormHeader.tsx
@@ -7,14 +7,19 @@ interface Step1FormHeaderProperties {
   isStep1: boolean;
 }
 
+const withAssociationsSubheading =
+  'Select the financial institution for which you are authorized to file to complete your user profile. Once your user profile has been associated with a financial institution in our database you will have access to the platform and can begin the filing process.';
+const noAssociationsSubheading =
+  'Indicate the financial institution for which you are authorized to file to complete your user profile. Once we have associated your user profile with a financial institution in our database you will have access to the platform and can begin the filing process.';
+
 function Step1FormHeader({ isStep1 }: Step1FormHeaderProperties): JSX.Element {
   return (
     <FormHeaderWrapper>
       <TextIntroduction
         heading='Complete your user profile'
-        subheading={`${
-          isStep1 ? 'Select' : 'Indicate'
-        } the financial institution for which you are authorized to file to complete your user profile. Once we have associated your user profile with a financial institution in our database you will have access to the platform and can begin the filing process.`}
+        subheading={
+          isStep1 ? withAssociationsSubheading : noAssociationsSubheading
+        }
         description={
           <>
             In order to begin using the platform you must have a Legal Entity
