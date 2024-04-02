473923639581820d1567c74c80039df9dd9f2de4
Meis <meissadia@gmail.com>
2024-04-02 12:28:32 -0600

    [fix][Homepage] Update hero content

--- src/pages/Filing/FilingHome.tsx
+++ src/pages/Filing/FilingHome.tsx
@@ -23,8 +23,8 @@ function Home(): ReactElement {
     <div id='filing-home'>
       <Layout.Main id='main' layout='2-1' bleedbar classes='main-layout'>
         <Hero
-          heading='Get started filing your small business lending data'
-          subheading='Covered financial institutions are required to compile, maintain, and report information about small business lending.'
+          heading='Get started filing your lending data'
+          subheading='Covered financial institutions and voluntary reporters to maintain, report, and publicly disclose information about lending.'
           backgroundColor='#EFF8FD'
         />
         <Layout.Wrapper>
