ab1abfac9c4cd088e94997732a124f55715a8579
shindigira <shindigira@gmail.com>
2024-04-03 14:42:56 -0400

    chore(uploadCsvAxios): has its own axiosInstance, interceptor is limited to just the uploadCsvAxios request

--- src/api/axiosService.ts
+++ src/api/axiosService.ts
@@ -1,48 +1,18 @@
 import type {
-  AxiosError,
   AxiosInstance,
-  AxiosRequestConfig,
-  AxiosResponse,
+  AxiosRequestConfig
 } from 'axios';
 import axios from 'axios';
-import type { UploadResponse } from 'types/filingTypes';
 
-const apiClient: AxiosInstance = axios.create({
-  baseURL: '',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const getAxiosInstance = (): AxiosInstance =>
+  axios.create({
+    baseURL: '',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-// Define a function to check if the response needs to be retried
-type AxiosResponseUploadStateType = AxiosResponse<
-  Partial<Pick<UploadResponse, 'state'>>
->;
-
-/** Used in `useGetSubmissionLatest` and LONGPOLL for validation after an upload * */
-function shouldRetry(response: AxiosResponseUploadStateType): boolean {
-  // Check if the response has a 'state' property equal to '"VALIDATION_IN_PROGRESS"'
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, @typescript-eslint/no-unused-expressions, prettier/prettier
-  return response.data?.state && response.data.state === 'VALIDATION_IN_PROGRESS';
-}
-
-const interceptor = apiClient.interceptors.response.use(
-  async (response: AxiosResponseUploadStateType) => {
-    // If the response doesn't need to be retried, resolve immediately
-    if (!shouldRetry(response)) {
-      return response;
-    } // Otherwise, retry the request
-    console.log(
-      'Validation STILL in-progress - Long Polling - RETRYING',
-      response,
-    );
-    return apiClient(response.config);
-  },
-  async (error: AxiosError) => {
-    // If an error occurs, reject immediately
-    throw error;
-  },
-);
+const apiClient = getAxiosInstance();
 
 type Methods = 'delete' | 'get' | 'head' | 'options' | 'patch' | 'post' | 'put';
 
