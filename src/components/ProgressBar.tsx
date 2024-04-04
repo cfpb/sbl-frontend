5798e5af0ed78e7c7931e61a846e56edee145899
shindigira <shindigira@gmail.com>
2024-04-04 11:19:13 -0400

    styling

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -6,9 +6,9 @@ interface ProgressBarProperties {
 
 function ProgressBar({ progress = Zero }: ProgressBarProperties): JSX.Element {
   return (
-    <div className='my-[1.875rem] h-6 w-full rounded-full bg-gray-200 dark:bg-gray-700'>
+    <div className='my-[1.875rem] h-6 w-full rounded-full bg-stepIndicatorIncomplete'>
       <div
-        className='box-border flex h-full max-w-full items-center justify-center rounded-full bg-blue-600 p-0.5 text-center text-lg font-medium leading-none text-blue-100'
+        className='box-border flex h-full max-w-full items-center justify-center rounded-full bg-stepIndicatorComplete p-0.5 text-center text-lg font-medium leading-none text-blue-100'
         style={{ width: `${progress}%` }}
       >
         {`${progress}%`}
