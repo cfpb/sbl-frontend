46971d5f6bc52662ab35b488e77b67456a216c67
Meis <meissadia@gmail.com>
2024-04-09 11:23:37 -0600

    fix: useMemo to avoid duplicate placeholder objects being generated

--- src/pages/Filing/FilingApp/FilingUpload.tsx
+++ src/pages/Filing/FilingApp/FilingUpload.tsx
@@ -1,15 +1,22 @@
+import { useMemo } from 'react';
 import { FilingStatusAsNumber } from 'utils/types';
 import { FilingStepWrapper } from './FilingStepWrapper';
 import { createMockFiling } from './FilingStepWrapper.helpers';
 
 function FilingUpload(): JSX.Element {
+  const mockFiling = useMemo(
+    () =>
+      createMockFiling({
+        status: FilingStatusAsNumber.SUBMISSION_STARTED,
+      }),
+    [],
+  );
+
   return (
     <FilingStepWrapper
       heading='Filing - Upload'
       hrefNext='/filing/errors'
-      currentFiling={createMockFiling({
-        status: FilingStatusAsNumber.SUBMISSION_STARTED,
-      })}
+      currentFiling={mockFiling}
     >
       UPLOAD CONTENT GOES HERE
     </FilingStepWrapper>
--- src/pages/Filing/FilingApp/FilingWarnings.tsx
+++ src/pages/Filing/FilingApp/FilingWarnings.tsx
@@ -1,16 +1,23 @@
+import { useMemo } from 'react';
 import { FilingStatusAsNumber } from 'utils/types';
 import { FilingStepWrapper } from './FilingStepWrapper';
 import { createMockFiling } from './FilingStepWrapper.helpers';
 
 function FilingWarnings(): JSX.Element {
+  const mockFiling = useMemo(
+    () =>
+      createMockFiling({
+        status: FilingStatusAsNumber.VALIDATION_WITH_WARNINGS,
+      }),
+    [],
+  );
+
   return (
     <FilingStepWrapper
       heading='Filing - Warnings'
       hrefPrevious='/filing/errors'
       hrefNext='/filing/contact'
-      currentFiling={createMockFiling({
-        status: FilingStatusAsNumber.VALIDATION_WITH_WARNINGS,
-      })}
+      currentFiling={mockFiling}
     >
       WARNING CONTENT GOES HERE
     </FilingStepWrapper>
