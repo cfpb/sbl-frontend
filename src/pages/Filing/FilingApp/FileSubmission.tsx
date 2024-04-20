34eaab82daa605800667307b9a8f3134ee2edb5d
shindigira <shindigira@gmail.com>
2024-04-20 07:29:24 -0700

    feat(Upload - Error Handling): Checks if a user tries to upload a file over a size limit (e.g. 2GB)

--- src/pages/Filing/FilingApp/FileSubmission.tsx
+++ src/pages/Filing/FilingApp/FileSubmission.tsx
@@ -21,6 +21,10 @@ import type { AxiosResponse } from 'axios';
 import { LoadingContent } from 'components/Loading';
 import type { SubmissionResponse } from 'types/filingTypes';
 import { filingInstructionsPage } from 'utils/common';
+import {
+  FILE_SIZE_LIMIT,
+  FILE_SIZE_LIMIT_ERROR_MESSAGE
+} from 'utils/constants';
 import FileDetailsUpload from './FileDetailsUpload';
 import FileDetailsValidation from './FileDetailsValidation';
 import { FileSubmissionState } from './FileSubmission.data';
@@ -97,10 +101,24 @@ export function FileSubmission(): JSX.Element {
     period_code: year,
     onSuccessCallback: handleAfterUpload,
   });
+
+  if (errorUpload)
+    console.log(
+      'errorUpload:',
+      errorUpload.message === FILE_SIZE_LIMIT_ERROR_MESSAGE,
+    );
   const onHandleSelectFile = (event: ChangeEvent<HTMLInputElement>): void => {
+    // NOTE: Test the user's selected file to both have data and be under the max size limit
+    const fileSizeTest = Boolean(
+      event.target.files?.[0] &&
+        // NOTE: Change to FILE_SIZE_LIMIT to FILE_SIZE_LIMIT_TEST to test 2MB instead of 2GB
+        (event.target.files[0].size > FILE_SIZE_LIMIT ||
+          event.target.files[0].size === 0),
+    );
+
     if (event.target.files && event.target.files.length > 0 && lei && year) {
       resetUpload();
-      mutateUpload({ file: event.target.files[0] });
+      mutateUpload({ file: event.target.files[0], fileSizeTest });
     }
 
     // NOTE: Workaround to allow uploading the same named file twice in a row
@@ -208,10 +226,10 @@ export function FileSubmission(): JSX.Element {
                   type='file'
                   ref={fileInputReference}
                   title={buttonLabel}
-                  // Relies on Button for visibility
+                  // Relies on the Button for functionality
                   className='invisible absolute inset-0 h-full w-full cursor-pointer opacity-0 disabled:cursor-not-allowed'
-                  id='file-input-specific'
-                  name='file-input-specific'
+                  id='file-input-upload'
+                  name='file-input-upload'
                   aria-hidden='true' // Hidden from screenreaders
                   accept='.csv'
                   onChange={onHandleSelectFile}
@@ -232,6 +250,7 @@ export function FileSubmission(): JSX.Element {
                   disabled={isLoadingUpload || isFetchingGetSubmissionLatest}
                 />
               </div>
+              {/* File Details section */}
               {isLoadingUpload ||
               dataUpload ||
               errorUpload ||
@@ -242,7 +261,6 @@ export function FileSubmission(): JSX.Element {
                   <Heading type='3'>Status</Heading>
                   {/* Upload Status Section - Statuses */}
                   <div className='flex flex-col gap-2'>
-                    {/* TODO: Clean-up ternary hells */}
                     <InlineStatus
                       statusOptions={[
                         { condition: isLoadingUpload, value: 'updating' },
@@ -269,6 +287,13 @@ export function FileSubmission(): JSX.Element {
                         {
                           condition: isLoadingUpload,
                           value: 'Upload in progress',
+                        },
+                        {
+                          condition:
+                            errorUpload &&
+                            errorUpload.message ===
+                              FILE_SIZE_LIMIT_ERROR_MESSAGE,
+                          value: FILE_SIZE_LIMIT_ERROR_MESSAGE,
                         },
                         { condition: errorUpload, value: 'Upload failed' },
                         {
