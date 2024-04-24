59673aae010a7b5ce0e588320730d9dde42fddcb
Meis <meissadia@gmail.com>
2024-04-24 14:42:44 -0600

    feat: [Sign and submit] Implement current design with live data

---
+++ src/pages/Filing/FilingApp/FilingSubmit.helpers.tsx
@@ -0,0 +1,109 @@
+import FormSectionWrapper from 'components/FormSectionWrapper';
+import SectionIntro from 'components/SectionIntro';
+import { Checkbox, WellContainer } from 'design-system-react';
+import type { ChangeEvent } from 'react';
+import { useParams } from 'react-router-dom';
+import type { FilingType, SubmissionResponse } from 'types/filingTypes';
+import { formatDateTimeShort } from 'utils/formatDateTime';
+import AddressStreet2 from '../ViewInstitutionProfile/AddressStreet2';
+import { DisplayField } from '../ViewInstitutionProfile/DisplayField';
+import InstitutionDataLabels from '../formHelpers';
+
+export function PointOfContactConfirm({
+  data,
+}: {
+  data: FilingType;
+}): JSX.Element {
+  const poc = data.contact_info;
+
+  return (
+    <FormSectionWrapper>
+      <SectionIntro heading='Filing point of contact'>
+        To make a change to your financial institution point of contact for this
+        filing return to Provide point of contact.
+      </SectionIntro>
+
+      <WellContainer className='u-mt30'>
+        <DisplayField label='First name' value={poc?.first_name} />
+        <DisplayField label='Last name' value={poc?.last_name} />
+        <DisplayField label='Email address' value={poc?.email} />
+        <DisplayField label='Phone number' value={poc?.phone} />
+        <DisplayField
+          label={InstitutionDataLabels.hqAddress}
+          value={
+            poc ? (
+              <>
+                {poc.hq_address_street_1}
+                {poc.hq_address_street_1 ? <br /> : null}
+                <AddressStreet2 street={poc.hq_address_street_2} />
+                {poc.hq_address_city ? <>{poc.hq_address_city},&nbsp;</> : null}
+                {poc.hq_address_state_code} {poc.hq_address_zip}
+              </>
+            ) : null
+          }
+        />
+      </WellContainer>
+    </FormSectionWrapper>
+  );
+}
+
+export function FileInformation({
+  data,
+}: {
+  data: SubmissionResponse;
+}): JSX.Element {
+  const { year } = useParams();
+
+  return (
+    <FormSectionWrapper>
+      <SectionIntro heading='File information'>
+        To make a change to your official file return to Upload file.
+      </SectionIntro>
+
+      <WellContainer className='u-mt30'>
+        <DisplayField label='File name' value={data.filename} />
+        <DisplayField
+          label='Uploaded by'
+          value={data.submitter.submitter_name}
+        />
+        <DisplayField
+          label='Uploaded on'
+          value={formatDateTimeShort(data.submission_time ?? '', 'fff')}
+        />
+        {/* TODO: Source of these quanitities? */}
+        <DisplayField label='Total verified warnings' value='TBD' />
+        <DisplayField label='Total loans/applications' value='TBD' />
+        <DisplayField label='Filing year' value={year} />
+      </WellContainer>
+    </FormSectionWrapper>
+  );
+}
+
+export function SignCertify({
+  name,
+  onChange,
+  value,
+}: {
+  name: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: boolean;
+}): JSX.Element {
+  return (
+    <FormSectionWrapper>
+      <SectionIntro heading='Sign and certify'>
+        To complete your official regulatory submission, select the checkbox
+        below to certify the accuracy and completeness of the data submitted,
+        then, click the “Submit filing” button.
+      </SectionIntro>
+
+      <WellContainer className='u-mt30'>
+        <Checkbox
+          id='sign-and-certify'
+          label={`I, ${name}, am an authorized representative of my institution with knowledge of the data submitted and I am certifying the accuracy and completeness of the data submitted.`}
+          checked={value}
+          onChange={onChange}
+        />
+      </WellContainer>
+    </FormSectionWrapper>
+  );
+}
--- src/pages/Filing/FilingApp/FilingSubmit.tsx
+++ src/pages/Filing/FilingApp/FilingSubmit.tsx
@@ -1,31 +1,171 @@
-import { Grid } from 'design-system-react';
+import { Alert, Button, Checkbox } from 'design-system-react';
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { FilingNavButtons } from './FilingNavButtons';
-import { FilingSteps } from './FilingSteps';
+import { useFilingAndSubmissionInfo } from 'utils/useFilingAndSubmissionInfo';
+import useInstitutionDetails from 'utils/useInstitutionDetails';
+import useUserProfile from 'utils/useUserProfile';
+import { AffiliateInformation } from '../ViewInstitutionProfile/AffiliateInformation';
+import { FinancialInstitutionDetails } from '../ViewInstitutionProfile/FinancialInstitutionDetails';
+import { IdentifyingInformation } from '../ViewInstitutionProfile/IdentifyingInformation';
+import { FilingStepWrapper } from './FilingStepWrapper';
+import {
+  FileInformation,
+  PointOfContactConfirm,
+  SignCertify,
+} from './FilingSubmit.helpers';
 
-function FilingSubmit(): JSX.Element {
+const subheading =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.';
+
+const description =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.';
+
+const initState = {
+  institution: false,
+  affiliate: false,
+  identifying: false,
+  poc: false,
+  certify: false,
+};
+
+const isSubmitEnabled = (checkboxValues: typeof initState): boolean =>
+  checkboxValues.institution &&
+  checkboxValues.affiliate &&
+  checkboxValues.identifying &&
+  checkboxValues.poc &&
+  checkboxValues.certify;
+
+export function FilingSubmit(): JSX.Element {
   const { lei, year } = useParams();
+  const [checkboxValues, setCheckboxValues] = useState({ ...initState });
+
+  const {
+    isError: userError,
+    isLoading: userLoading,
+    data: user,
+  } = useUserProfile();
+
+  const {
+    data: institution,
+    isLoading: institutionLoading,
+    isError: institutionError,
+  } = useInstitutionDetails(lei);
+
+  const {
+    filing,
+    submission,
+    isLoading: filingLoading,
+    error,
+  } = useFilingAndSubmissionInfo({
+    lei,
+    filingPeriod: year,
+  });
+
+  if (filingLoading || institutionLoading || userLoading)
+    return <div>Loading...</div>;
+
+  if (error || userError || institutionError)
+    return (
+      <>
+        <Alert status='error' message={userError} isVisible={!!userError} />
+        <Alert
+          status='error'
+          message={institutionError}
+          isVisible={!!institutionError}
+        />
+        <Alert status='error' message={error} isVisible={!!error} />
+      </>
+    );
+
+  const onCheckboxUpdate =
+    (id: string) =>
+    (event: ChangeEvent<HTMLInputElement>): void => {
+      setCheckboxValues({ ...checkboxValues, [id]: event.target.checked });
+    };
+
+  const onClear = (): void => setCheckboxValues({ ...initState });
 
   return (
-    <>
-      <FilingSteps />
-      <Grid.Wrapper center>
-        <Grid.Row>
-          <Grid.Column width={8} className='u-mt15'>
-            SIGN & SUBMIT CONTENT GOES HERE
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={8} className='u-mt15'>
-            <FilingNavButtons
-              hrefPrevious={`/filing/${year}/${lei}/contact`}
-              hrefNext={`/filing/${year}/${lei}/done`}
-              isStepComplete // TODO: Derive actual step status
-            />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid.Wrapper>
-    </>
+    <FilingStepWrapper
+      heading='Sign and Submit'
+      subheading={subheading}
+      description={description}
+    >
+      <FinancialInstitutionDetails
+        heading='Confirm financial institution details'
+        data={institution}
+        isDomainsVisible={false}
+      />
+      <div className='u-mt30'>
+        <Checkbox
+          id='fi-details'
+          label='Check box action so a user can confirm that details are correct'
+          checked={checkboxValues.institution}
+          onChange={onCheckboxUpdate('institution')}
+        />
+      </div>
+
+      <IdentifyingInformation
+        heading='Confirm identifying information'
+        data={institution}
+      />
+      <div className='u-mt30'>
+        <Checkbox
+          id='identifying-info'
+          label='Check box action so a user can confirm that details are correct'
+          checked={checkboxValues.identifying}
+          onChange={onCheckboxUpdate('identifying')}
+        />
+      </div>
+
+      <AffiliateInformation
+        heading='Confirm affiliate information'
+        data={institution}
+      />
+      <div className='u-mt30'>
+        <Checkbox
+          id='affiliate-info'
+          label='Check box action so a user can confirm that details are correct'
+          checked={checkboxValues.affiliate}
+          onChange={onCheckboxUpdate('affiliate')}
+        />
+      </div>
+
+      <PointOfContactConfirm data={filing} />
+      <div className='u-mt30'>
+        <Checkbox
+          id='poc'
+          label='Check box action so a user can confirm that details are correct'
+          checked={checkboxValues.poc}
+          onChange={onCheckboxUpdate('poc')}
+        />
+      </div>
+
+      <FileInformation data={submission} />
+
+      <SignCertify
+        name={user.name.length > 0 ? user.name : user.email}
+        onChange={onCheckboxUpdate('certify')}
+        value={checkboxValues.certify}
+      />
+
+      <div className='u-mt30'>
+        <Button
+          label='Submit filing'
+          type='submit'
+          onClick={onClear}
+          className='mr-5'
+          disabled={!isSubmitEnabled(checkboxValues)}
+        />
+        <Button
+          label='Clear form'
+          asLink
+          onClick={onClear}
+          appearance='warning'
+        />
+      </div>
+    </FilingStepWrapper>
   );
 }
 
