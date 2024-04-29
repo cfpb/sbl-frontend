0e45de1a5ff7b42fc76a72c69eb8f7a930031e0f
Meis <meissadia@gmail.com>
2024-04-29 10:36:46 -0600

    task: More extensive Markdown testing - need plugin for tables, strikethrough, advanced elements

--- src/MarkdownTest.tsx
+++ src/MarkdownTest.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable prettier/prettier */
 import { Grid } from 'design-system-react';
 import type { JSXElement } from 'design-system-react/dist/types/jsxElement';
 import Markdown from 'react-markdown';
@@ -30,6 +32,24 @@ export function MarkdownText(): JSXElement {
               );
             },
           )}
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={8} className='u-mt15'>
+          <Markdown>
+            {`# H1\n## H2\n### H3`}
+          </Markdown>
+          <Markdown>{`**bold text**\n\n*italicized text*\n\n> blockquote\n`}</Markdown>
+          <Markdown>{`~~strikethrough~~`}</Markdown>
+          <Markdown>{`1. First item\n2. Second item\n4. Third item`}</Markdown>
+          <Markdown>{`- First item\n- Second item\n- Third item`}</Markdown>
+          {/* // eslint-disable-next-line react/jsx-curly-brace-presence */}
+          <Markdown>{"`code`"}</Markdown>
+          <Markdown>{"---"}</Markdown>
+          <Markdown>{"[Markdown Guide](https://www.markdownguide.org)"}</Markdown>
+          <Markdown>{"![alt text](https://www.markdownguide.org/assets/images/tux.png)"}</Markdown>
+          <Markdown>{`## Table\n\n| Syntax | Description |\n| ----------- | ----------- |\n| Header | Title |\n| Paragraph | Text |\n\n`}</Markdown>
+          <Markdown>{"## Codeblock\n\n```\n{\n'firstName': 'John',\n  'lastName': 'Smith',\n  'age': 25\n}\n```"}</Markdown>
         </Grid.Column>
       </Grid.Row>
     </Grid.Wrapper>
