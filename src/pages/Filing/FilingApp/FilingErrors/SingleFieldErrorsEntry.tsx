7a4bc32cce19bb6524ac139d4b48c359011394f9
shindigira <shindigira@gmail.com>
2024-04-30 08:07:57 -0700

    enhancement(single-field-errors): Started table implementation

--- src/pages/Filing/FilingApp/FilingErrors/SingleFieldErrorsEntry.tsx
+++ src/pages/Filing/FilingApp/FilingErrors/SingleFieldErrorsEntry.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'components/Link';
-import { Heading } from 'design-system-react';
+import { Heading, List, ListItem, Table } from 'design-system-react';
 
 interface SingleFieldErrorsEntryProperties {
   singleErrorObject: unknown;
@@ -11,11 +11,28 @@ function SingleFieldErrorsEntry({
   const validationId = singleErrorObject.validation.id;
   const validationLink = singleErrorObject.validation.fig_link;
   const validationName = singleErrorObject.validation.name;
+  const validationDescription = singleErrorObject.validation.description;
+  const column3Header = singleErrorObject.records[0].fields[0].name;
   return (
-    <>
-      <Link href={validationLink}>{validationId}</Link>
+    <div className='mb-[2.8125rem]'>
+      <Link target='_blank' href={validationLink}>
+        <Heading type='3'>{validationId}</Heading>
+      </Link>
       <Heading type='4'>{validationName}</Heading>
-    </>
+      <List>
+        <ListItem>{validationDescription}</ListItem>
+      </List>
+      <Table
+        className='w-full'
+        // caption='Table caption describing the data'
+        columns={['Row', 'Unique identifier (uid)', column3Header]}
+        rows={[
+          ['Row 1, Column 1', 'Row 1, Column 2', 'Row 1, Column 3'],
+          ['Row 2, Column 1', 'Row 2, Column 2', 'Row 2, Column 3'],
+          ['Row 3, Column 1', 'Row 3, Column 2', 'Row 3, Column 3'],
+        ]}
+      />
+    </div>
   );
 }
 
