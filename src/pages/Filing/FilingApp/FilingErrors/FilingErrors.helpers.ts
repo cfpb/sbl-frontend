7d9872027a196d9848527a666f75cb42a882261c
shindigira <shindigira@gmail.com>
2024-05-02 12:11:07 -0700

    typescript update

--- src/pages/Filing/FilingApp/FilingErrors/FilingErrors.helpers.ts
+++ src/pages/Filing/FilingApp/FilingErrors/FilingErrors.helpers.ts
@@ -1,10 +1,30 @@
-export const getErrorsWarnings = (property = 'logic_errors', data) => {
-  const summary = {
+import type {
+  Detail,
+  SubmissionResponse,
+  ValidationResults,
+} from 'types/filingTypes';
+
+interface GetErrorsWarningsProperties {
+  data: SubmissionResponse | undefined;
+  property: keyof ValidationResults;
+}
+
+interface SummaryResults {
+  singles: Detail[];
+  multis: Detail[];
+  registers: Detail[];
+}
+
+export const getErrorsWarnings = ({
+  data,
+  property = 'logic_errors',
+}: GetErrorsWarningsProperties): SummaryResults => {
+  const summary: SummaryResults = {
     singles: [],
     multis: [],
     registers: [],
   };
-  console.log('datadatadata', data);
+
   console.log(
     'data?.validation_result?.[property]',
     data?.validation_results?.[property],
@@ -12,41 +32,27 @@ export const getErrorsWarnings = (property = 'logic_errors', data) => {
 
   if (!data?.validation_results?.[property]) return summary;
 
-  summary.singles = data.validation_results[property]?.details.filter(
-    object => object?.validation?.scope === 'single-field',
+  summary.singles = data.validation_results[property].details.filter(
+    object => object.validation.scope === 'single-field',
   );
 
-  summary.multis = data.validation_results[property]?.details.filter(
-    object => object?.validation?.scope === 'multi-field',
+  summary.multis = data.validation_results[property].details.filter(
+    object => object.validation.scope === 'multi-field',
   );
 
-  summary.registers = data.validation_results[property]?.details.filter(
-    object => object?.validation?.scope === 'register',
+  summary.registers = data.validation_results[property].details.filter(
+    object => object.validation.scope === 'register',
   );
 
   return summary;
 };
 
-export const formatMultis = summaryType => {
-  for (const validationObject of summaryType) {
-    validationObject.formattedRecords = new Map();
-    for (const recordObject of validationObject.records) {
-      for (const fieldObject of recordObject.fields) {
-        if (!validationObject.formattedRecords.has(fieldObject.name)) {
-          validationObject.formattedRecords.set(fieldObject.name, []);
-        }
-        validationObject.formattedRecords
-          .get(fieldObject.name)
-          .push([recordObject.record_no, recordObject.uid, fieldObject.value]);
-      }
-    }
-  }
-};
-
-export const getErrorsWarningsSummary = data => {
-  const syntaxErrors = getErrorsWarnings('syntax_errors', data);
-  const logicErrors = getErrorsWarnings('logic_errors', data);
-  const logicWarnings = getErrorsWarnings('logic_warnings', data);
+export const getErrorsWarningsSummary = (
+  data: SubmissionResponse,
+): Record<string, Detail[] | SummaryResults> => {
+  const syntaxErrors = getErrorsWarnings({ data, property: 'syntax_errors' });
+  const logicErrors = getErrorsWarnings({ data, property: 'logic_errors' });
+  const logicWarnings = getErrorsWarnings({ data, property: 'logic_warnings' });
   const registerErrors = [...logicErrors.registers];
   const syntaxErrorsSingle = [...syntaxErrors.singles];
   const syntaxErrorsMulti = [...syntaxErrors.multis];
