d8a277874b327229e58be9f999f07df9f554d8fb
Meis <meissadia@gmail.com>
2024-05-03 12:20:49 -0600

    feat: [Warnings] Integrate Sigmunds warnings implementation

--- src/pages/Filing/FilingApp/FilingWarnings.tsx
+++ src/pages/Filing/FilingApp/FilingWarnings.tsx
@@ -1,26 +1,29 @@
 import submitWarningsAccept from 'api/requests/submitWarningsVerified';
 import useSblAuth from 'api/useSblAuth';
-import classnames from 'classnames';
+import FormButtonGroup from 'components/FormButtonGroup';
 import FormWrapper from 'components/FormWrapper';
+import { LoadingContent } from 'components/Loading';
 import {
   Alert,
   Button,
   Checkbox,
   Heading,
-  Icon,
   List,
   ListLink,
   TextIntroduction,
   WellContainer,
 } from 'design-system-react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import type { SubmissionResponse } from 'types/filingTypes';
 import { FileSubmissionState } from 'types/filingTypes';
 import useGetSubmissionLatest from 'utils/useGetSubmissionLatest';
 import useInstitutionDetails from 'utils/useInstitutionDetails';
+import FieldSummary from './FieldSummary';
+import { getErrorsWarningsSummary } from './FilingErrors/FilingErrors.helpers';
 import { NavigationPrevious } from './FilingNavButtons';
 import { FilingSteps } from './FilingSteps';
+import FilingWarningsAlerts from './FilingWarnings/FilingWarningsAlerts';
 import InstitutionHeading from './InstitutionHeading';
 
 const isSubmissionAccepted = (submission?: SubmissionResponse): boolean => {
@@ -40,18 +43,27 @@ function FilingWarnings(): JSX.Element {
     isLoading: isSubmissionLoading,
     isError: errorSubmissionFetch,
   } = useGetSubmissionLatest({ lei, filingPeriod: year });
-
   const {
     data: institution,
     isLoading: isInstitutionLoading,
     isError: errorInstitutionFetch,
   } = useInstitutionDetails(lei);
 
-  const institutionName = isInstitutionLoading
-    ? 'Loading...'
-    : errorInstitutionFetch
-      ? ''
-      : institution.name;
+  const formattedData = useMemo(
+    () => getErrorsWarningsSummary(submission),
+    [submission],
+  );
+
+  if (isSubmissionLoading || isInstitutionLoading) return <LoadingContent />;
+
+  const { logicWarningsMulti, logicWarningsSingle } = formattedData;
+
+  const hasWarnings = [logicWarningsMulti, logicWarningsSingle].some(
+    array => array.length > 0,
+  );
+
+  // TODO: Display error Alert when Institution data unavailable?
+  const institutionName = errorInstitutionFetch ? '' : institution.name;
 
   const onClickCheckbox = (): void => {
     setBoxChecked(!boxChecked);
@@ -87,8 +99,6 @@ function FilingWarnings(): JSX.Element {
 
   const isLoading = formSubmitLoading || isSubmissionLoading;
 
-  const hasWarnings = submission?.validation_results?.logic_warnings.count > 0;
-
   const isVerified =
     isSubmissionAccepted(submission) || boxChecked || !hasWarnings;
 
@@ -114,38 +124,61 @@ function FilingWarnings(): JSX.Element {
               report to determine if the values flagged with warning validations
               require action. If there are underlying problems, make the
               corrections, and upload a new file.
-              <div id='warnings-listlinks' className='mt-[1.875rem]'>
-                <List isLinks>
-                  <ListLink href='#'>Download validation report</ListLink>
-                  <ListLink href={`/filing/${year}/${lei}/upload`}>
-                    Upload a new file
-                  </ListLink>
-                </List>
-              </div>
+              {!errorSubmissionFetch && (
+                <div id='resolve-warnings-listlinks' className='mt-[1.875rem]'>
+                  <List isLinks>
+                    <ListLink href='#'>Download validation report</ListLink>
+                    <ListLink href={`/filing/${year}/${lei}/upload`}>
+                      Upload a new file
+                    </ListLink>
+                  </List>
+                </div>
+              )}
             </>
           }
         />
-        {!errorSubmissionFetch &&
-        (!hasWarnings || isSubmissionAccepted(submission)) ? (
-          <Alert
-            className='mb-[2.8125rem] [&_div]:max-w-[41.875rem] [&_p]:max-w-[41.875rem]'
-            message='All warnings have successfully been verified'
-            status='success'
-          />
-        ) : null}
-        {errorSubmissionFetch ? (
-          <Alert
-            className='mb-[2.8125rem] [&_div]:max-w-[41.875rem] [&_p]:max-w-[41.875rem]'
-            message='Error fetching your submission data'
-            status='error'
-          >
-            Please reload the page to try again.
-          </Alert>
-        ) : null}
+        <FilingWarningsAlerts
+          {...{
+            errorState: hasWarnings && !isVerified,
+            errorGetSubmissionLatest: errorSubmissionFetch,
+          }}
+        />
         <div className='u-mt45 u-mb60'>
-          &lt;&lt; PAGE CONTENT GOES HERE &gt;&gt;
+          {!errorSubmissionFetch && (
+            <>
+              {/* SINGLE-FIELD WARNINGS */}
+              {hasWarnings ? (
+                <FieldSummary
+                  id='single-field-warnings'
+                  heading={`Single-field warnings found: ${logicWarningsSingle.length}`}
+                  fieldArray={logicWarningsSingle}
+                  bottomMargin
+                >
+                  Each single-field validation pertains to only one specific
+                  field in each record. These validations check that the data
+                  held in an individual field match the values that are
+                  expected.
+                </FieldSummary>
+              ) : null}
+              {hasWarnings ? (
+                <>
+                  {/* MULTI-FIELD WARNINGS */}
+                  <FieldSummary
+                    id='multi-field-warnings'
+                    heading={`Multi-field warnings found: ${logicWarningsMulti.length}`}
+                    fieldArray={logicWarningsMulti}
+                    bottomMargin
+                  >
+                    Multi-field validations check that the values of certain
+                    fields make sense in combination with other values in the
+                    same record.
+                  </FieldSummary>
+                </>
+              ) : null}
+            </>
+          )}
         </div>
-        {hasWarnings ? (
+        {hasWarnings && !errorSubmissionFetch ? (
           <WellContainer className='u-mt30'>
             <Heading type='3'>Verify all warnings to continue</Heading>
             <Checkbox
@@ -154,14 +187,6 @@ function FilingWarnings(): JSX.Element {
               onChange={onClickCheckbox}
               checked={isVerified}
               disabled={isLoading || isSubmissionAccepted(submission)}
-              helperText={
-                isSubmissionLoading ? (
-                  <span className='mx-2'>
-                    <Icon name='updating' className='mr-2' />
-                    Loading...
-                  </span>
-                ) : null
-              }
             />
           </WellContainer>
         ) : null}
@@ -174,21 +199,21 @@ function FilingWarnings(): JSX.Element {
             Please click the &quot;Save and continue&quot; button to try again.
           </Alert>
         ) : null}
-        <div className='u-mb60'>
+        <FormButtonGroup isFilingStep>
           <NavigationPrevious
             label='Go back to previous step'
             href={`/filing/${year}/${lei}/errors`}
           />
           <Button
             appearance='primary'
-            className={classnames('mt-[1.875rem]')}
+            className='mt-[1.875rem]'
             iconRight={formSubmitLoading ? 'updating' : 'right'}
             label='Save and continue'
             onClick={onFormSubmit}
             size='default'
             disabled={!canContinue || formSubmitLoading}
           />
-        </div>
+        </FormButtonGroup>
       </FormWrapper>
     </>
   );
