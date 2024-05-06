63353967e0f0682321542e764e8e0be6b571e2d4
shindigira <shindigira@gmail.com>
2024-05-06 11:37:39 -0700

    step2 message fixed

--- src/pages/Filing/FilingApp/FilingErrors/index.tsx
+++ src/pages/Filing/FilingApp/FilingErrors/index.tsx
@@ -109,11 +109,22 @@ function FilingErrors(): JSX.Element {
             subheading='First, our system checks that each value in your register meets data type and format requirements. We are unable to accurately detect consequent errors or warnings until each record in your register passes these syntax checks.'
             description={
               <>
-                If applicable, review the tables below or download the
-                validation report to identify the specific issues that caused
-                the validation to fail. Once you’ve identified the underlying
-                problems, make the corrections to your register, and upload a
-                new file.
+                {isStep2 ? (
+                  <>
+                    Next, our system checks your register to confirm that there
+                    are no inconsistencies or mistakes in how the information is
+                    organized or represented. Your register must pass these
+                    logic checks to continue to the next step.
+                  </>
+                ) : (
+                  <>
+                    If applicable, review the tables below or download the
+                    validation report to identify the specific issues that
+                    caused the validation to fail. Once you’ve identified the
+                    underlying problems, make the corrections to your register,
+                    and upload a new file.
+                  </>
+                )}
                 {!errorGetSubmissionLatest && (
                   <div id='resolve-errors-listlinks' className='mt-[1.875rem]'>
                     <List isLinks>
