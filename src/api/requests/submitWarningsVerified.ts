3554322e724d6bdd345bc452c2872d27211ed51f
Meis <meissadia@gmail.com>
2024-05-07 11:08:55 -0600

    fix: [Warnings] Make required params required

--- src/api/requests/submitWarningsVerified.ts
+++ src/api/requests/submitWarningsVerified.ts
@@ -2,27 +2,18 @@ import { request } from 'api/axiosService';
 import type { SblAuthProperties } from 'api/useSblAuth';
 import type { SubmissionResponse } from 'types/filingTypes';
 
-interface Options {
-  lei?: string;
-  filingPeriod?: string;
-  submissionId?: number;
+interface Parameters {
+  lei: string;
+  filingPeriod: string;
+  submissionId: number;
 }
 
 const submitWarningsAccept = async (
   auth: SblAuthProperties,
-  options: Options,
+  { lei, filingPeriod, submissionId }: Parameters,
 ): Promise<null> => {
-  const { lei, filingPeriod, submissionId } = options;
-
-  /* Params are defined as optional because of TS complaining about content
-     that gets fetched/loaded, but we need to ensure they all exist
-  */
-  if (![lei, filingPeriod, submissionId].every(Boolean)) {
-    console.log(`submitWarningsAccept: Missing required parameter`, options);
-    return new Promise(resolve => {
-      resolve(null);
-    });
-  }
+  if (![lei, filingPeriod, submissionId].every(Boolean))
+    throw new Error('submitWarningsAccept: Missing required parameter');
 
   return request<Partial<SubmissionResponse>, null>({
     url: `/v1/filing/institutions/${lei}/filings/${filingPeriod}/submissions/${submissionId}/accept`,
