8dafb4b3185d5abebabe09b7be4ac7bfb95ac819
shindigira <shindigira@gmail.com>
2024-05-13 14:16:36 -0700

    focus style on validation link

--- src/pages/Filing/FilingApp/FieldEntry.tsx
+++ src/pages/Filing/FilingApp/FieldEntry.tsx
@@ -95,7 +95,7 @@ function FieldEntry({ fieldObject }: FieldEntryProperties): JSX.Element {
       <div className='validation-info-section mb-[1.875rem] max-w-[41.875rem]'>
         <Link target='_blank' href={validationLink}>
           <Heading
-            className='inline-block border-x-0 border-b-[1px] border-t-0 border-dotted'
+            className='focus: inline-block border-x-0 border-b-[1px] border-t-0 border-dotted outline-1 focus:border-solid focus:outline-dotted'
             type='3'
           >
             {validationId}
