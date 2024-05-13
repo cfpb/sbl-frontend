330590520465ad759d0eda027a88c4c1db3dd71d
Meis <meissadia@gmail.com>
2024-05-13 14:04:21 -0600

    task: [Page][Warnings] Update Nav buttons

--- src/pages/Filing/FilingApp/FilingWarnings/index.tsx
+++ src/pages/Filing/FilingApp/FilingWarnings/index.tsx
@@ -5,7 +5,6 @@ import FormWrapper from 'components/FormWrapper';
 import { LoadingContent } from 'components/Loading';
 import {
   Alert,
-  Button,
   Checkbox,
   Heading,
   TextIntroduction,
@@ -21,7 +20,7 @@ import useInstitutionDetails from 'utils/useInstitutionDetails';
 import FieldSummary from '../FieldSummary';
 import { getErrorsWarningsSummary } from '../FilingErrors/FilingErrors.helpers';
 import FilingFieldLinks from '../FilingFieldLinks';
-import { NavigationPrevious } from '../FilingNavButtons';
+import { FilingNavButtons } from '../FilingNavButtons';
 import { FilingSteps } from '../FilingSteps';
 import InstitutionHeading from '../InstitutionHeading';
 import FilingWarningsAlerts, {
@@ -108,6 +107,8 @@ function FilingWarnings(): JSX.Element {
       setFormSubmitError(response); // Display error alert
     }
   };
+
+  const onPreviousClick = (): void => navigate(`/filing/${year}/${lei}/errors`);
 
   return (
     <>
@@ -200,18 +201,12 @@ function FilingWarnings(): JSX.Element {
         </Alert>
 
         <FormButtonGroup isFilingStep>
-          <NavigationPrevious
-            label='Go back to previous step'
-            href={`/filing/${year}/${lei}/errors`}
-          />
-          <Button
-            appearance='primary'
-            className='mt-[1.875rem]'
-            iconRight={formSubmitLoading ? 'updating' : 'right'}
-            label='Save and continue'
-            onClick={onFormSubmit}
-            size='default'
-            disabled={!canContinue || formSubmitLoading}
+          <FilingNavButtons
+            classNameButtonContainer='u-mb0'
+            onPreviousClick={onPreviousClick}
+            onNextClick={onFormSubmit}
+            isNextDisabled={!canContinue || formSubmitLoading}
+            isLoading={formSubmitLoading}
           />
         </FormButtonGroup>
       </FormWrapper>
