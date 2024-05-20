7d71ba1f45fd5087593a107334136f562d925649
Meis <meissadia@gmail.com>
2024-05-20 16:45:56 -0600

    fix: [Types of FI] Error header message adjustment

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -9,6 +9,8 @@ export enum FormFieldsHeaderError {
   tin = 'Enter your Federal Taxpayer Identification Number (TIN)',
   name = "Enter your financial institution's name",
   lei = "Enter your financial institution's Legal Entity Identifier (LEI)",
+  sbl_institution_types = 'Select your type of financial institution',
+  sbl_institution_types_other = 'Enter a type of financial institution for "Other"',
 }
 
 // Used in react-select format (potentially can be removed)
