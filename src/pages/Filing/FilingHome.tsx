23629c6b5be4f7c128437311c58bba247512a2c4
Bill Himmelsbach <whimmels@gmail.com>
2024-05-28 10:24:08 -0700

    feat(beta-language): make a component, add to shared home

--- src/pages/Filing/FilingHome.tsx
+++ src/pages/Filing/FilingHome.tsx
@@ -1,8 +1,8 @@
 import useSblAuth from 'api/useSblAuth';
 import AdditionalResources from 'components/AdditionalResources';
+import BetaAndLegalNotice from 'components/BetaAndLegalNotice';
 import { Link, ListLink } from 'components/Link';
 import {
-  Alert,
   Button,
   Divider,
   Heading,
@@ -30,39 +30,7 @@ function Home(): ReactElement {
         />
         <Layout.Wrapper>
           <Layout.Content className='content_main u-mb15'>
-            <Alert
-              className='mb-[2.813rem]'
-              message={
-                <>
-                  <Heading type='2' className='h4 mb-[0.313rem]'>
-                    This is a beta for the small business lending data
-                    submission platform
-                  </Heading>
-                  <Paragraph>
-                    Thank you for participating. The data submission platform is
-                    available to upload, test, and validate data. All uploaded
-                    data is for testing purposes only and may be removed at any
-                    time. For questions or feedback,{' '}
-                    <Link href='mailto:SBLHelp@cfpb.gov?subject=[BETA] Home page: Questions or suggestions'>
-                      email our support staff
-                    </Link>
-                    .
-                  </Paragraph>
-                  <Paragraph>
-                    In light of court orders in ongoing litigation, the CFPB
-                    plans to issue an interim final rule to extend the
-                    compliance dates of the small business lending rule.{' '}
-                    <Link href='https://www.consumerfinance.gov/1071-rule/'>
-                      Find out more
-                    </Link>
-                  </Paragraph>
-                </>
-              }
-              status='warning'
-              // TODO: allow setting to strip heading formatting in Alerts post-mvp
-              // @ts-expect-error - See issue: https://github.com/cfpb/design-system-react/issues/351
-              headingLevel={null}
-            />
+            <BetaAndLegalNotice />
             <div className='mb-[2.813rem]'>
               <Heading type='2'>Sign in with Login.gov</Heading>
               <Paragraph>
