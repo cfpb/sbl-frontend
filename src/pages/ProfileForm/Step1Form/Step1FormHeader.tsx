a357b1949117fac9e944af64f3d9a480abc4438d
Meis <meissadia@gmail.com>
2024-05-29 15:21:36 -0600

    task: [Complete user profile] Update GLIEF language

--- src/pages/ProfileForm/Step1Form/Step1FormHeader.tsx
+++ src/pages/ProfileForm/Step1Form/Step1FormHeader.tsx
@@ -23,9 +23,9 @@ function Step1FormHeader({ isStep1 }: Step1FormHeaderProperties): JSX.Element {
         description={
           <Paragraph>
             In order to begin using the platform you must have a Legal Entity
-            Identifier (LEI) for your financial institution. Visit the{' '}
-            <Link href={gleifLink}>Global LEI Foundation (GLEIF)</Link> website
-            for more information on how to obtain an LEI.
+            Identifier (LEI) for your financial institution. If your
+            organization does not have an LEI, visit the{' '}
+            <Link href={gleifLink}>Global LEI Foundation (GLEIF)</Link> website.
           </Paragraph>
         }
       />
