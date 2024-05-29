5ba3bf8e6bd6403b3dbbbd4f1019e1046692c80b
Meis <meissadia@gmail.com>
2024-05-29 14:26:41 -0600

    styles: [Filing landing] Underline Institution links

--- src/components/AssociatedInstitution.tsx
+++ src/components/AssociatedInstitution.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/require-default-props */
 import { Link } from 'components/Link';
-import { Icon, ListItem } from 'design-system-react';
+import { Icon } from 'design-system-react';
 import type { InstitutionDetailsApiType } from 'types/formTypes';
 
 interface FirstLast {
@@ -14,49 +14,40 @@ export function AssociatedInstitution({
   isFirst,
   isLast,
 }: FirstLast & InstitutionDetailsApiType): JSX.Element {
-  let baseStylesLink = `inline-block w-full border-pacific visited:border-teal border-dashed border-0 border-t-[1px] py-[.625rem]`;
+  let baseStylesLink = `inline-block w-full border-pacific visited:border-teal border-dashed border-0 border-t-[1px] py-[.625rem] mb-0`;
   if (isLast) baseStylesLink += ' border-b-[1px]';
 
-  let desktopStylesLink = 'lg:border-0 lg:pb-0';
+  let desktopStylesLink = ' lg:border-0 lg:pb-0';
   if (isFirst) desktopStylesLink += ' lg:pt-0';
   if (isLast) desktopStylesLink += ' lg:pb-0';
 
-  // Error
-  if (!name || !lei)
-    return (
-      <ListItem key={lei} className='associated-institution my-0'>
-        <Link
-          href='mailto:SBLHelp@cfpb.gov?subject=[BETA] Associated institutions: Missing "Name" or "LEI"'
-          className={`${baseStylesLink} ${desktopStylesLink} font-medium`}
-        >
-          <Icon
-            className='mr-[5px] text-[#20aa3f]'
-            isPresentational
-            name='approved'
-            withBg
-          />
-          <span className='mr-[10px] font-normal text-[#101820]'>Approved</span>
-          Missing institution details, email our support staff.
-        </Link>
-      </ListItem>
-    );
+  let href =
+    'mailto:SBLHelp@cfpb.gov?subject=[BETA] Associated institutions: Missing "Name" or "LEI"';
+  let text = 'Missing institution details, email our support staff.';
+
+  // We have all the required information to build a link
+  if (name && lei) {
+    href = `/institution/${lei}`;
+    text = `${name} | ${lei}`;
+  }
 
   return (
-    <ListItem key={lei} className='associated-institution my-0'>
+    <li className={baseStylesLink + desktopStylesLink}>
+      <Icon
+        className='mr-[5px] text-[#20aa3f]'
+        isPresentational
+        name='approved'
+        withBg
+      />
+      <span className='mr-[0.625rem] font-normal text-[#101820]'>Approved</span>
       <Link
-        href={lei ? `/institution/${lei}` : `/404`}
-        className={`${baseStylesLink} ${desktopStylesLink} font-medium`}
+        href={href}
+        key={lei}
+        className='associated-institution  my-0 font-medium max-lg:border-b-0'
       >
-        <Icon
-          className='mr-[5px] text-[#20aa3f]'
-          isPresentational
-          name='approved'
-          withBg
-        />
-        <span className='mr-[10px] font-normal text-[#101820]'>Approved</span>
-        {`${name} | ${lei}`}
+        {text}
       </Link>
-    </ListItem>
+    </li>
   );
 }
 
