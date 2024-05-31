c7f4ce1c89ad0bc77e5616354a542e58107bc438
Meis <meissadia@gmail.com>
2024-05-31 14:41:59 -0600

    fix: [AssociatedInstitution] Leverage DSRs ListLink component to get borders to render  correctly

--- src/components/AssociatedInstitution.tsx
+++ src/components/AssociatedInstitution.tsx
@@ -1,26 +1,12 @@
 /* eslint-disable react/require-default-props */
-import { Link } from 'components/Link';
+import { ListLink } from 'components/Link';
 import { Icon } from 'design-system-react';
 import type { InstitutionDetailsApiType } from 'types/formTypes';
-
-interface FirstLast {
-  isFirst?: boolean;
-  isLast?: boolean;
-}
 
 export function AssociatedInstitution({
   name,
   lei,
-  isFirst,
-  isLast,
-}: FirstLast & InstitutionDetailsApiType): JSX.Element {
-  let baseStylesLink = `inline-block w-full border-pacific visited:border-teal border-dashed border-0 border-t-[1px] py-[.625rem] mb-0 font-medium`;
-  if (isLast) baseStylesLink += ' border-b-[1px]';
-
-  let desktopStylesLink = ' lg:border-0 lg:pb-0';
-  if (isFirst) desktopStylesLink += ' lg:pt-0';
-  if (isLast) desktopStylesLink += ' lg:pb-0';
-
+}: InstitutionDetailsApiType): JSX.Element {
   let href =
     'mailto:SBLHelp@cfpb.gov?subject=[BETA] Associated institutions: Missing "Name" or "LEI"';
   let text = 'Missing institution details, email our support staff.';
@@ -32,17 +18,15 @@ export function AssociatedInstitution({
   }
 
   return (
-    <li className={baseStylesLink + desktopStylesLink}>
+    <ListLink href={href} key={lei} hasIcon>
       <Icon
-        className='mr-[5px] text-pacific'
+        className='cf-icon-svg-wrapper mr-[5px]'
         isPresentational
         name='approved'
         withBg
       />
-      <Link href={href} key={lei} className='max-lg:border-b-0'>
-        {text}
-      </Link>
-    </li>
+      <span className='a-link_text'>{text}</span>
+    </ListLink>
   );
 }
 
