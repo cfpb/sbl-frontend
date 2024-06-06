37aa9bca2c055f7877bd1e4f274e6e268db7b97f
shindigira <shindigira@gmail.com>
2024-06-06 13:12:29 -0700

    revert(errors/warnings): upload new file button --> link

--- src/pages/Filing/FilingApp/FilingFieldLinks.tsx
+++ src/pages/Filing/FilingApp/FilingFieldLinks.tsx
@@ -1,8 +1,7 @@
 import downloadValidationReport from 'api/requests/downloadValidationReport';
 import useSblAuth from 'api/useSblAuth';
-import Button from 'components/Button';
-import { List, ListItem } from 'design-system-react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'components/Link';
+import { Button, List } from 'design-system-react';
 import type { FilingPeriodType } from 'types/filingTypes';
 
 interface FilingFieldLinksProperties {
@@ -21,37 +20,22 @@ function FilingFieldLinks({
   ...others
 }: FilingFieldLinksProperties & JSX.IntrinsicElements['div']): JSX.Element {
   const auth = useSblAuth();
-  const navigate = useNavigate();
 
   const onHandleDownloadClick = async (): Promise<void> => {
     await downloadValidationReport({ auth, lei, filingPeriod, submissionId });
   };
-
-  const onUploadNewFile = (): void =>
-    navigate(`/filing/${filingPeriod}/${lei}/upload`);
-
   return (
     <div id={id} className={`mt-[1.875rem] ${className}`} {...others}>
       <List className='flex items-center gap-[0.9375rem]' isLinks>
-        <ListItem>
-          <Button
-            label='Download report'
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={onHandleDownloadClick}
-          />
-        </ListItem>
-        <ListItem>
-          {/* 
-            TODO: Replace button used as link - I couldn't get the button-like-styling right
-            https://github.com/cfpb/sbl-frontend/issues/575
-           */}
-          <Button
-            appearance='secondary'
-            label='Upload a new file'
-            className='mb-[0.5rem]'
-            onClick={onUploadNewFile}
-          />
-        </ListItem>
+        <Button
+          label='Download report'
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onClick={onHandleDownloadClick}
+          iconRight='download'
+        />
+        <Link href={`/filing/${filingPeriod}/${lei}/upload`}>
+          Upload a new file
+        </Link>
       </List>
     </div>
   );
