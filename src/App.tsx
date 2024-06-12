5400abc0103df1eb234bc270ab686d290f660277
Bill Himmelsbach <whimmels@gmail.com>
2024-06-12 10:52:14 -0400

    fix(auth): silent renew auth token between refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import FilingWarnings from 'pages/Filing/FilingApp/FilingWarnings';
 import UpdateFinancialProfile from 'pages/Filing/UpdateFinancialProfile';
 import ViewUserProfile from 'pages/Filing/ViewUserProfile';
 import type { ReactElement } from 'react';
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import {
   BrowserRouter,
   Navigate,
@@ -30,6 +30,7 @@ import {
   Route,
   Routes,
   useLocation,
+  useNavigate,
 } from 'react-router-dom';
 import type { UserProfileType } from 'types/filingTypes';
 import getIsRoutingEnabled, {
@@ -108,6 +109,20 @@ export function NavItem({
 function BasicLayout(): ReactElement {
   const headerLinks = [...useHeaderAuthLinks()];
   const location = useLocation();
+
+  const auth = useSblAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    return auth.events.addAccessTokenExpiring(() => {
+      try {
+        auth.signinSilent();
+      } catch (error) {
+        navigate('/');
+      }
+    });
+  }, [auth.events, auth.signinSilent]);
+
 
   const isFilingPage = Boolean(location.pathname.startsWith('/filing/'));
 
