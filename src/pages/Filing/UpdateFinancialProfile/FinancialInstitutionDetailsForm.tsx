716179ed57dbd2b4ef5451e94b4bc3fa339f7da4
Meis <meissadia@gmail.com>
2024-06-13 09:50:42 -0600

    [Pre-clearance] Update institution profile (#683)
    Close #682 
    
    |Section|Shot|
    |---|---|
    |View Financial institution details|![Screenshot 2024-06-11 at 11 19
    30 AM](https://github.com/cfpb/sbl-frontend/assets/2592907/ac0bc137-fbbf-4532-aa72-f2bb5a118ffb)|
    |Update Financial institution details|![Screenshot 2024-06-11 at 11 19
    39 AM](https://github.com/cfpb/sbl-frontend/assets/2592907/19f4ccfb-f9d7-442a-ae11-bc3c2d8736d4)|
    |Type of FI helper
    text|![update-types-helper-text](https://github.com/cfpb/sbl-frontend/assets/2592907/289e28a2-2a9e-4a58-9fd2-f76602ff1e08)|
    |Parent entity intro|<img width="790" alt="Screenshot 2024-06-10 at 9 43
    56 AM"
    src="https://github.com/cfpb/sbl-frontend/assets/2592907/588df7d0-08c7-4555-b465-db4869ef8c07">|

--- src/pages/Filing/UpdateFinancialProfile/FinancialInstitutionDetailsForm.tsx
+++ src/pages/Filing/UpdateFinancialProfile/FinancialInstitutionDetailsForm.tsx
@@ -1,16 +1,21 @@
 import type { JSXElement } from 'design-system-react/dist/types/jsxElement';
+import type { ReactNode } from 'react';
 import type { InstitutionDetailsApiType } from 'types/formTypes';
 import { FinancialInstitutionDetails } from '../ViewInstitutionProfile/FinancialInstitutionDetails';
 
 export default function FinancialInstitutionDetailsForm({
   data,
+  description,
 }: {
   data: InstitutionDetailsApiType;
+  // eslint-disable-next-line react/require-default-props
+  description?: ReactNode;
 }): JSXElement {
   return (
     <FinancialInstitutionDetails
       heading='Financial institution details'
       data={data}
+      description={description}
     />
   );
 }
