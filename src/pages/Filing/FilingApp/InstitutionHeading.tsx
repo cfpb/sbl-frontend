c161f67cfb672912a49aebf861fb2dcf0fc5da56
Meis <meissadia@gmail.com>
2024-06-18 15:09:41 -0600

    fix: [Filing home] Add LEI to Institution cards & wrap long concontent

--- src/pages/Filing/FilingApp/InstitutionHeading.tsx
+++ src/pages/Filing/FilingApp/InstitutionHeading.tsx
@@ -4,21 +4,18 @@ import type { InstitutionDataType } from './InstitutionCard.types';
 // Format the Institution name + LEI
 function InstitutionHeading({
   name,
+  lei,
   filingPeriod,
   eyebrow = false,
   // eslint-disable-next-line react/require-default-props
 }: InstitutionDataType & { eyebrow?: boolean }): JSX.Element {
   const content: (number | string)[] = [];
-  for (const item of [name, filingPeriod]) {
+  for (const item of [name, lei, filingPeriod, name, lei, filingPeriod]) {
     if (item) {
       content.push(item);
     }
   }
   const contentUsed = content.filter(Boolean).join(`${'  '}|${'  '}`);
-  return (
-    <Heading className='whitespace-pre' type={eyebrow ? 'eyebrow' : '5'}>
-      {contentUsed}
-    </Heading>
-  );
+  return <Heading type={eyebrow ? 'eyebrow' : '5'}>{contentUsed}</Heading>;
 }
 export default InstitutionHeading;
