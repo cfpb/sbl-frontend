876daf65493a29ee9199671828075df250db9e96
Bill Himmelsbach <whimmels@gmail.com>
2024-08-08 06:04:09 -0700

    fix(content): adjust warning and poc copy (#855)
    Updated copy on the warning and point of contact pages in accordance
    with usability feedback.
    
    - update warning page copy for "Verify flagged register values"
    - make point of contact form errors only use "There was a problem
    updating your point of contact information" for the alert header
    - remove now unused variable `hasFormRegexErrors`
    
    1. Has the copy changed?
    2. Does the site still work?
    
    **Warning page**
    ![Screenshot 2024-08-07 at 11 51 29
    AM](https://github.com/user-attachments/assets/2b3fdf74-a00d-475a-80a9-91006afabc9c)
    
    **Point of contact**
    ![Screenshot 2024-08-07 at 11 46 39
    AM](https://github.com/user-attachments/assets/19eaf2b9-165b-415a-bb77-b70f04b3a8da)

--- src/pages/PointOfContact/index.tsx
+++ src/pages/PointOfContact/index.tsx
@@ -201,11 +201,6 @@ function PointOfContact(): JSX.Element {
     }
   };
 
-  const hasFormRegexErrors = Object.keys(formErrors).some(
-    key =>
-      formErrors[key as keyof PointOfContactSchema]?.type === 'invalid_string',
-  );
-
   // TODO: Redirect the user if the filing period or lei are not valid
 
   if (isLoading) return <LoadingContent />;
@@ -243,11 +238,7 @@ function PointOfContact(): JSX.Element {
           />
         ) : null}
         <FormErrorHeader<PointOfContactSchema, PocFormHeaderErrorsType>
-          alertHeading={
-            hasFormRegexErrors
-              ? 'There was a problem updating your point of contact information'
-              : 'You must provide all required point of contact information to save and continue'
-          }
+          alertHeading='There was a problem updating your point of contact information'
           errors={formErrors}
           id={formErrorHeaderId}
           formErrorHeaderObject={PocFormHeaderErrors}
@@ -258,7 +249,7 @@ function PointOfContact(): JSX.Element {
             You are required to complete all fields with the exception of the
             street address lines labeled optional. Your point of contact
             information will not be saved until you provide all required
-            information and click &quot;Save and continue.&quot;
+            information and continue to the next step.
           </SectionIntro>
         </div>
         {/*  eslint-disable-next-line @typescript-eslint/no-misused-promises */}
