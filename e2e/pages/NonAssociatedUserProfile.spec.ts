28c87d0d6ba2b01eec2d3ec70e1cfc8586c0ef62
shindigira <shindigira@gmail.com>
2024-09-10 08:23:24 -0700

    enhancement: checks url for no-associations

--- e2e/pages/NonAssociatedUserProfile.spec.ts
+++ e2e/pages/NonAssociatedUserProfile.spec.ts
@@ -1,11 +1,16 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/testFixture';
 
-// this is just an example test (e2e tests should be way longer than this)
-test('Unauthenticated homepage: CFPB logo link goes to consumerfinance.gov', async ({
-  page,
-}) => {
-  await page.goto('/');
-  await page.getByLabel('Home').click();
-  await expect(page).toHaveURL('https://www.consumerfinance.gov/');
+const expectedNoAssociationsUrl =
+  'http://localhost:8899/profile/complete/no-associations';
+
+// Test with isNonAssociatedUser true
+test.use({ isNonAssociatedUser: true });
+test('Complete User Profile -- no associations', async ({ page }) => {
+  // await page.goto('/');
+  // await page.getByLabel('Home').click();
+  // await expect(page).toHaveURL('https://www.consumerfinance.gov/');
+
+  await expect(page.locator('h1')).toContainText('Complete your user profile');
+  await expect(page).toHaveURL(expectedNoAssociationsUrl);
 });
