c898892e7848c86d3212639f6d866bcd9928f193
shindigira <shindigira@gmail.com>
2024-09-19 12:20:59 -0700

    using a smaller logic errors file to reduce the validation time

--- e2e/pages/filing-app/uploadFile/completeUploadLogicErrors.spec.ts
+++ e2e/pages/filing-app/uploadFile/completeUploadLogicErrors.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from '@playwright/test';
-import path from 'node:path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import { test } from '../../../fixtures/testFixture';
 
 test('Resolve Errors (Logic)', async ({ page, navigateToUploadFile }) => {
@@ -16,7 +17,7 @@ test('Resolve Errors (Logic)', async ({ page, navigateToUploadFile }) => {
       await fileChooser.setFiles(
         path.join(
           __dirname,
-          '/../../../test-data/sample-sblar-files/logic-errors_medium.csv',
+          '/../../../test-data/sample-sblar-files/logic-errors_single&multi_and_warnings.csv',
         ),
       );
       await expect(page.getByText('File upload in progress')).toBeVisible();
@@ -36,12 +37,12 @@ test('Resolve Errors (Logic)', async ({ page, navigateToUploadFile }) => {
       await expect(page.locator('h1')).toContainText(
         'Resolve errors (syntax)',
         {
-          timeout: 20_000,
+          timeout: 30_000,
         },
       );
       await expect(
         page.getByText('Your register contains no syntax errors'),
-      ).toBeVisible({ timeout: 20_000 });
+      ).toBeVisible({ timeout: 40_000 });
     });
 
     await test.step('Verify Resolve Errors (logic) and number of errors', async () => {
@@ -50,9 +51,44 @@ test('Resolve Errors (Logic)', async ({ page, navigateToUploadFile }) => {
       await expect(page.locator('#error-header-alert')).toContainText(
         'Your register contains logic errors',
       );
-      await expect(page.locator('#register-level-errors')).toContainText(
-        'Register-level errors: 10,126 found',
+      await expect(page.locator('#single-field-errors')).toContainText(
+        'Single-field errors: 1 found',
       );
+      await expect(page.locator('#register-level-errors')).toContainText(
+        'Register-level errors: 2 found',
+      );
+      await expect(page.locator('#multi-field-errors')).toContainText(
+        'Multi-field errors: 52 found',
+      );
+    });
+
+    await test.step('Verify downloadable report', async () => {
+      // Set up download path
+      const downloadPath = path.resolve(__dirname, 'downloads');
+      if (!fs.existsSync(downloadPath)) {
+        fs.mkdirSync(downloadPath);
+      }
+
+      // Set up listener for the download event
+      const downloadPromise = page.waitForEvent('download');
+      await page.getByRole('button', { name: 'Download report' }).click();
+      const download = await downloadPromise;
+
+      const suggestedFilename = await download.suggestedFilename();
+
+      // Save the download to a specific location
+      const downloadFilePath = path.join(downloadPath, suggestedFilename);
+      await download.saveAs(downloadFilePath);
+
+      // Wait for the download to complete
+      await download.path();
+
+      // Verify the file exists
+      expect(fs.existsSync(downloadFilePath)).toBeTruthy();
+
+      // Verify file size
+      const fileSize = fs.statSync(downloadFilePath).size;
+      expect(fileSize).toBeGreaterThan(0); // Ensure the file is not empty
     });
   });
 });
