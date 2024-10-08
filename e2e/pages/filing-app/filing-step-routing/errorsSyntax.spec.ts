7d832f99113d842f9cff7c7301e25f6331a68ab2
Meis <meissadia@gmail.com>
2024-10-08 10:19:38 -0600

    [e2e] (Errors - Syntax) Verify users cannot skip steps

---
+++ e2e/pages/filing-app/filing-step-routing/errorsSyntax.spec.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-await-in-loop */
+
+import { expect } from '@playwright/test';
+import { test } from '../../../fixtures/testFixture';
+
+const testLabel = 'Filing step routing (Errors: Syntax)';
+
+const currentStepPath = '/error';
+
+const userShouldNotAccess = [
+  '/errors/errors-logic',
+  '/warnings',
+  '/contact',
+  '/submit',
+];
+
+const afterRedirectHeading = 'Resolve errors (syntax)';
+const afterRedirectURL = /.*errors\/errors-syntax$/;
+
+test(
+  testLabel,
+  async ({ page, navigateToSyntaxErrorsAfterSyntaxErrorsUpload }) => {
+    test.slow();
+
+    navigateToSyntaxErrorsAfterSyntaxErrorsUpload;
+
+    const [baseURL] = page.url().split(currentStepPath);
+
+    for (const step of userShouldNotAccess) {
+      await test.step(`${testLabel}: Verify user cannot access ${step}`, async () => {
+        await page.goto(baseURL + step);
+        await expect(page).toHaveURL(afterRedirectURL);
+        await expect(page.getByText(afterRedirectHeading)).toBeVisible();
+      });
+    }
+  },
+);
