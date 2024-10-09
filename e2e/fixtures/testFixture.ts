7d24d9f79ff8522f7e14c17c8f80f8d6d91bbcb2
Meis <meissadia@gmail.com>
2024-10-09 13:56:54 -0600

    [e2e] (task) Integrate new uploadFile utility function

--- e2e/fixtures/testFixture.ts
+++ e2e/fixtures/testFixture.ts
@@ -197,29 +197,15 @@ export const test = baseTest.extend<{
   ) => {
     navigateToUploadFile;
     await test.step('Upload file: navigate to Syntax Errors page after only errors upload', async () => {
-      await test.step('Upload file: upload small file with only syntax errors (errors-page-1-syntax-few.csv)', async () => {
-        await expect(page.locator('h2')).toContainText(
-          'Select a file to upload',
-        );
-        const fileChooserPromise = page.waitForEvent('filechooser');
-        await page.getByLabel('Select a .csv file to upload').click();
-        const fileChooser = await fileChooserPromise;
-        await fileChooser.setFiles(
-          path.join(
-            __dirname,
-            '../test-data/sample-sblar-files/errors-page-1-syntax-few.csv',
-          ),
-        );
-        await expect(page.getByText('File upload in progress')).toBeVisible();
-        await expect(page.getByText('File upload successful')).toBeVisible({
-          timeout: 10_000,
-        });
-        await expect(
-          page.getByText('Validation checks in progress'),
-        ).toBeVisible({ timeout: 10_000 });
-        await expect(
-          page.getByText('Errors were found in your file'),
-        ).toBeVisible({ timeout: 60_000 });
+      await uploadFile({
+        testUsed: test,
+        pageUsed: page,
+        newUpload: true,
+        testTitle:
+          'Upload file: upload small file with only syntax errors (errors-page-1-syntax-few.csv)',
+        filePath:
+          '../test-data/sample-sblar-files/errors-page-1-syntax-few.csv',
+        resultMessage: ResultUploadMessage.logic,
       });
 
       await test.step('Upload file: navigate to Resolve errors (syntax) with no errors after upload', async () => {
@@ -243,29 +229,15 @@ export const test = baseTest.extend<{
   ) => {
     navigateToUploadFile;
     await test.step('Upload file: navigate to Logic Errors page after only errors upload', async () => {
-      await test.step('Upload file: upload small file with only logic errors (errors-page-2-logic-few.csv)', async () => {
-        await expect(page.locator('h2')).toContainText(
-          'Select a file to upload',
-        );
-        const fileChooserPromise = page.waitForEvent('filechooser');
-        await page.getByLabel('Select a .csv file to upload').click();
-        const fileChooser = await fileChooserPromise;
-        await fileChooser.setFiles(
-          path.join(
-            __dirname,
-            '../test-data/sample-sblar-files/errors-page-2-logic-few.csv',
-          ),
-        );
-        await expect(page.getByText('File upload in progress')).toBeVisible();
-        await expect(page.getByText('File upload successful')).toBeVisible({
-          timeout: 10_000,
-        });
-        await expect(
-          page.getByText('Validation checks in progress'),
-        ).toBeVisible({ timeout: 10_000 });
-        await expect(
-          page.getByText('Errors were found in your file'),
-        ).toBeVisible({ timeout: 60_000 });
+      await uploadFile({
+        testUsed: test,
+        pageUsed: page,
+        newUpload: true,
+        testTitle:
+          'Upload file: upload small file with only logic errors (errors-page-2-logic-few.csv)',
+        filePath:
+          '../test-data/sample-sblar-files/logic-errors_single&multi_and_warnings.csv',
+        resultMessage: ResultUploadMessage.logic,
       });
 
       await test.step('Upload file: navigate to Resolve errors (syntax) with no errors after upload', async () => {
