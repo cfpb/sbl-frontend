1a1d6641234c20eaf0c5d92f43661dd556a322ad
Meis <meissadia@gmail.com>
2024-10-16 12:31:46 -0600

    [e2e] unavailableApis.spec - Refactor + add extended timeouts

--- e2e/utils/blockApi.ts
+++ e2e/utils/blockApi.ts
@@ -1,4 +1,7 @@
 import type { Page } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { test } from '../fixtures/testFixture';
+import { TIMEOUT_XL } from './timoutConstants';
 
 export async function blockApi(
   page: Page,
@@ -18,3 +21,58 @@ export async function blockApi(
     await page.reload();
   }
 }
+
+/**
+ * Verify the process:
+ *   Block API -> Confirm error boundary -> Unblock API -> Confirm Filing app
+ */
+
+interface VerifyApiBlockThenUnblockProperties {
+  endpointPath: RegExp | string;
+  expectedHeading: string;
+  page: Page;
+  endpointLabel: string;
+  timeoutOverride?: number;
+}
+
+export const verifyApiBlockThenUnblock = async ({
+  endpointPath,
+  expectedHeading,
+  page,
+  endpointLabel,
+  timeoutOverride = TIMEOUT_XL,
+}: VerifyApiBlockThenUnblockProperties) => {
+  const timeoutOption = { timeout: timeoutOverride };
+
+  await expect(page.locator('h1'), 'h1 is correct').toContainText(
+    expectedHeading,
+    timeoutOption,
+  );
+
+  // Block API Call
+  await test.step(`Block API: ${endpointLabel}`, async () => {
+    await blockApi(page, endpointPath, true);
+  });
+
+  // Confirm Error Boundary
+  await test.step('Error Boundary is visible', async () => {
+    await test.step('Refresh page', async () => {
+      await page.reload();
+    });
+
+    await expect(page.locator('h1'), 'h1 is correct').toContainText(
+      'An unknown error occurred',
+      timeoutOption,
+    );
+  });
+
+  // Unblock API Call
+  await test.step('Unblock API', async () => {
+    await blockApi(page, endpointPath, false);
+
+    await expect(page.locator('h1'), 'h1 is correct').toContainText(
+      expectedHeading,
+      timeoutOption,
+    );
+  });
+};
