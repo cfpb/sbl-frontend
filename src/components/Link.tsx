e9309938fa6ea563dc2e96e2eaf7d3f32ee950f9
Meis <meissadia@gmail.com>
2024-10-21 11:45:24 -0600

    feat: [Link] Programmatically determine `internal vs external` links task: [Link] Refactor to simplify implied usage of RouterLink

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,42 +4,10 @@
 import type { LinkProperties as DesignSystemReactLinkProperties } from 'design-system-react';
 import {
   Link as DesignSystemReactLink,
-  ListLink as DesignSystemReactListLink,
-  Icon,
+  LinkText,
+  ListItem,
 } from 'design-system-react';
-
-const getIsLinkExternal = (url: string | undefined): boolean => {
-  if (url === undefined) {
-    return false;
-  }
-  const externalUriSchemes = ['http', 'mailto:', 'tel:', 'sms:', 'ftp:'];
-  let isExternal = false;
-  for (const uriScheme of externalUriSchemes) {
-    if (url.startsWith(uriScheme)) {
-      isExternal = true;
-    }
-  }
-  return isExternal;
-};
-
-const getIsRouterUsageInferred = (
-  href: string | undefined,
-  isRouterLink: boolean | undefined,
-): boolean => isRouterLink === undefined && !getIsLinkExternal(href);
-
-const getIsRouterLink = (
-  href: string | undefined,
-  isRouterLink: boolean | undefined,
-): boolean => {
-  const isRouterUsageInferred = getIsRouterUsageInferred(href, isRouterLink);
-  if (isRouterUsageInferred) {
-    return true;
-  }
-  if (isRouterLink === undefined) {
-    return false;
-  }
-  return isRouterLink;
-};
+import { IconExternalLink, isExternalLink } from './Link.utils';
 
 interface LinkProperties extends DesignSystemReactLinkProperties {
   // design system react's Link component correctly allows undefined values without defaultProps
@@ -58,54 +26,36 @@ export function Link({
   className,
   ...others
 }: LinkProperties): JSX.Element {
-  const isInternalLink = getIsRouterLink(href, isRouterLink);
+  const hrefString = String(href);
+  const isExternal = isExternalLink(hrefString);
   const otherProperties: LinkProperties = { ...others };
-  let cname = className ?? '';
-
   let icon = null;
-  if (!isInternalLink) {
-    cname += ' whitespace-nowrap';
+
+  if (isExternal) {
     otherProperties.target = '_blank'; // Open link in new tab
-    icon = (
-      <>
-        {' '}
-        <Icon name='external-link' className='link-icon-override-color' />
-      </>
-    );
+    otherProperties.hasIcon = true; // Underline text, not icon
+    icon = <IconExternalLink />; // Display icon
   }
 
   return (
     <DesignSystemReactLink
       href={href}
-      isRouterLink={isInternalLink}
-      hasIcon={!isInternalLink}
-      className={cname}
+      isRouterLink={isRouterLink ?? !isExternal}
+      className={className}
       {...otherProperties}
     >
-      {children}
+      <LinkText>{children}</LinkText>
       {icon}
     </DesignSystemReactLink>
   );
 }
 
-export function ListLink({
-  href,
-  isRouterLink,
-  children,
-  ...others
-}: LinkProperties): JSX.Element {
-  const isInternalLink = getIsRouterLink(href, isRouterLink);
-  const otherProperties: LinkProperties = { ...others };
-
-  if (!isInternalLink) otherProperties.target = '_blank'; // Open link in new tab
-
+export function ListLink({ children, ...others }: LinkProperties): JSX.Element {
   return (
-    <DesignSystemReactListLink
-      href={href}
-      isRouterLink={getIsRouterLink(href, isRouterLink)}
-      {...others}
-    >
-      {children}
-    </DesignSystemReactListLink>
+    <ListItem>
+      <Link {...others} type='list'>
+        {children}
+      </Link>
+    </ListItem>
   );
 }
---
+++ src/components/Link.utils.tsx
@@ -0,0 +1,36 @@
+import { Icon } from 'design-system-react';
+import type { ReactElement } from 'react';
+
+/**
+ * Programmatically determine if a link is external to the CFPB sphere of websites
+ */
+export const isExternalLink = (targetUrl: string): boolean => {
+  let parsed;
+
+  try {
+    parsed = new URL(targetUrl);
+  } catch {
+    return false; // Internal targets will fail parsing (ex. '/home')
+  }
+
+  const externalProtocols = ['http', 'mailto:', 'tel:', 'sms:', 'ftp:'];
+  if (externalProtocols.includes(parsed.protocol)) return true;
+
+  // Any subdomain of consumerfinance.gov or the current host
+  const isInternal = new RegExp(
+    `([\\S]*\\.)?(((consumerfinance|cf)\\.gov)|(${window.location.host}))`,
+  );
+  if (!isInternal.test(parsed.host)) return true;
+
+  return false;
+};
+
+// External link icon w/ spacing
+export function IconExternalLink(): ReactElement {
+  return (
+    <>
+      {' '}
+      <Icon name='external-link' className='link-icon-override-color' />
+    </>
+  );
+}
