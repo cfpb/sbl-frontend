ce3fc426d36a360b428656255491f0a422c047ab
Tanner Ricks <182143365+tanner-ricks@users.noreply.github.com>
2024-10-29 13:44:23 -0500

    [e2e] Make tests runnable against dev deployment (#1029)
    Made tests configurable through environment variables in preparation for
    running against the dev deployment
    
    ## Changes
    
    - Created environment variables for test
    - Added Constants file to pull in the environment variables for test
    - Updated hard coded locations to use the values in the Constants file
    instead
    - Updated playwright config to pull in the base url as an environment
    variable
    
    ## How to test this PR
    
    1. Read the Notes
    2. Pull the branch
    3. Relaunch the stack if necessary
    4. Run the e2e tests and verify that they all still pass
    5. Play with the envars if you wish and see that the behavior changes as
    the envars do
    
    ## Notest
    
    - There appears to be a test failure in InstitutionProfile.spec.ts that
    is coming from Design System React.
      - If you encounter this, you can ignore it.

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,9 @@ const config: PlaywrightTestConfig = {
   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'http://localhost:8899/',
+    baseURL: `${
+      process.env.SBL_PLAYWRIGHT_TEST_TARGET ?? 'http://localhost:8899'
+    }/`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
