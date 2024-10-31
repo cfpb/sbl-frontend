ba68692a27cdb248c4d514195cb1fad0cca69734
Meis <meissadia@gmail.com>
2024-10-31 15:03:46 -0600

    feat: [View Institution profile] Display alert for "Not provided" fields

--- src/pages/Filing/ViewInstitutionProfile/DisplayField.tsx
+++ src/pages/Filing/ViewInstitutionProfile/DisplayField.tsx
@@ -1,11 +1,38 @@
 import classNames from 'classnames';
-import { Heading } from 'design-system-react';
+import { AlertFieldLevel, Heading, Link } from 'design-system-react';
 import type { ReactNode } from 'react';
+import { useParams } from 'react-router-dom';
+import InstitutionDataLabels from '../formHelpers';
 import './DisplayField.less';
 
 export const NOT_AVAILABLE = 'Not available';
 export const NOT_APPLICABLE = 'Not applicable';
 export const NOT_PROVIDED = 'Not provided';
+
+function LinkUpdateInstitutionProfile(): JSX.Element {
+  const { lei } = useParams();
+
+  return (
+    <Link href={`/institution/${lei}/update`}>
+      update your financial institution profile
+    </Link>
+  );
+}
+
+const NotProvidedAlertMessage = {
+  [InstitutionDataLabels.tin]: (
+    <p>
+      You must provide your TIN to file. For instructions on how to provide your
+      TIN visit <LinkUpdateInstitutionProfile />.
+    </p>
+  ),
+  [InstitutionDataLabels.fiType]: (
+    <p>
+      You must provide your type of financial institution to file. To provide
+      this information visit <LinkUpdateInstitutionProfile />.
+    </p>
+  ),
+};
 
 export interface DisplayFieldProperties {
   label?: ReactNode;
@@ -20,6 +47,9 @@ export function DisplayField({
   className,
   fallbackValue,
 }: DisplayFieldProperties): JSX.Element {
+  const resultingValue = value ?? fallbackValue;
+  const showAlert = resultingValue === NOT_PROVIDED;
+
   return (
     <div className={classNames('display-field', className)}>
       {label ? (
@@ -27,7 +57,12 @@ export function DisplayField({
           {label}
         </Heading>
       ) : undefined}
-      <p className='u-mt10 break-all'>{value ?? fallbackValue}</p>
+      <p className='u-mt10 break-all'>{resultingValue}</p>
+      <AlertFieldLevel
+        status='warning'
+        isVisible={showAlert}
+        message={NotProvidedAlertMessage[label as string]}
+      />
     </div>
   );
 }
