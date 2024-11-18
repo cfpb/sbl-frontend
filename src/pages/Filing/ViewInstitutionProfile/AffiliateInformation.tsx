711aa8b9724a92ff9020bee3d783a0ba1a5be60e
Meis <meissadia@gmail.com>
2024-11-18 16:07:08 -0700

    [Update and View institution profile] Language updates

--- src/pages/Filing/ViewInstitutionProfile/AffiliateInformation.tsx
+++ src/pages/Filing/ViewInstitutionProfile/AffiliateInformation.tsx
@@ -12,8 +12,8 @@ import { DisplayField, NOT_APPLICABLE } from './DisplayField';
 const defaultDescription = (
   <>
     If you wish to update the following data, contact your Local Operating Unit
-    (LOU) or visit the <Links.FederalReserveBoard />. If you wish to provide
-    only your parent entity’s name, where no LEI or RSSD ID exists, submit a
+    (LOU) or visit the <Links.FederalReserveBoard />. If you have parent
+    entities with no LEI or RSSD ID and wish to provide their names, submit a
     request to <Links.UpdateInstitutionProfile />.
   </>
 );
