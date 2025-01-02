5f946ee975b0e8c33b076e13557a327cbdd37a05
Meis <meissadia@gmail.com>
2025-01-02 14:19:04 -0700

    [e2e] Verify link targets: Logic errors

--- e2e/pages/filing-app/filing-home/VerifyLinkTargets.spec.ts
+++ e2e/pages/filing-app/filing-home/VerifyLinkTargets.spec.ts
@@ -39,3 +39,30 @@ test('Upload file', async ({ page, navigateToUploadFile }) => {
   });
   await expectLinkOpensNewTab(readAboutFiling);
 });
+
+test('Logic errors', async ({
+  page,
+  navigateToLogicErrorsAfterLogicErrorsUpload,
+}) => {
+  await navigateToLogicErrorsAfterLogicErrorsUpload;
+  await page.goto('/filing');
+
+  await expect(page.locator('h1')).toContainText(
+    'File your small business lending data',
+  );
+
+  const continueFiling = await page.getByRole('button', {
+    name: 'Continue filing',
+  });
+  await expect(continueFiling).toBeEnabled();
+
+  const fig = await page.getByRole('link', {
+    name: 'filing instructions guide for small business lending data',
+  });
+  await expectLinkOpensNewTab(fig);
+
+  const readAboutFiling = await page.getByRole('link', {
+    name: 'Read about data validations',
+  });
+  await expectLinkOpensNewTab(readAboutFiling);
+});
--- e2e/pages/filing-app/filing-step-routing/errorsLogic.spec.ts
+++ e2e/pages/filing-app/filing-step-routing/errorsLogic.spec.ts
@@ -1,4 +1,8 @@
 import { test } from '../../../fixtures/testFixture';
+import {
+  expectLinkOpensNewTab,
+  expectLinkOpensSameTab,
+} from '../../../utils/openLink';
 import { checkSnapshot } from '../../../utils/snapshotTesting';
 import { verifyRedirects } from './_shared';
 
@@ -28,3 +32,32 @@ test(
     await checkSnapshot(page);
   },
 );
+
+test('Verify link targets (Errors: Logic)', async ({
+  page,
+  navigateToLogicErrorsAfterLogicErrorsUpload,
+}) => {
+  navigateToLogicErrorsAfterLogicErrorsUpload;
+
+  // Same tab
+  const uploadNewFile = await page.getByRole('link', {
+    name: 'Upload a new file',
+  });
+  await expectLinkOpensSameTab(uploadNewFile);
+
+  // New tab
+  const figErrorDefinitions = await page
+    .locator('.validation-info-section a')
+    .all();
+  for (const [index, figPage] of figErrorDefinitions.entries()) {
+    // eslint-disable-next-line no-await-in-loop
+    await test.step(`FIG error link #${index + 1}`, async () => {
+      await expectLinkOpensNewTab(figPage);
+    });
+  }
+
+  const figDataValidation = page.getByRole('link', {
+    name: 'section 4, "Data validation"',
+  });
+  await expectLinkOpensNewTab(figDataValidation);
+});
