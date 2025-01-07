74227a6a8ef3bb94efb5e28c052fcdecda37dd5e
Meis <meissadia@gmail.com>
2025-01-07 14:57:14 -0700

    e2e: Snapshot - Ignore footer

--- src/components/FooterCfGovWrapper.tsx
+++ src/components/FooterCfGovWrapper.tsx
@@ -3,7 +3,7 @@ import { FooterCfGov } from 'design-system-react';
 function FooterCfGovWrapper(): JSX.Element {
   // TODO: Choose between:
   // Maximizing the midsection white space with 'mt-auto' or maximizing the gray space in the footer with 'flex-1
-  return <FooterCfGov className='flex-1' />;
+  return <FooterCfGov className='snapshot-ignore flex-1' />;
 }
 
 export default FooterCfGovWrapper;
