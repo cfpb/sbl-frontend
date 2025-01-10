f08fe5051369c3ae2827c02a05735e2dd5b3fb07
tanner-ricks <182143365+tanner-ricks@users.noreply.github.com>
2025-01-10 13:23:47 -0600

    Section Intro coverage

--- src/components/__tests__/SectionIntro.test.tsx
+++ src/components/__tests__/SectionIntro.test.tsx
@@ -5,9 +5,27 @@ describe('<SectionIntro />', () => {
   it('Renders expected content', async () => {
     render(
       <SectionIntro data-testid='test' id='test'>
-        <></>
+        {undefined}
       </SectionIntro>,
     );
     expect(screen.getByTestId('test')).toBeInTheDocument();
+  });
+
+  it('Renders expected heading content', async () => {
+    render(
+      <SectionIntro data-testid='test' id='test' heading='test heading'>
+        {undefined}
+      </SectionIntro>,
+    );
+    expect(screen.getByText('test heading')).toBeInTheDocument();
+  });
+
+  it('Renders expected child content', async () => {
+    render(
+      <SectionIntro data-testid='test' id='test'>
+        <span>test content</span>
+      </SectionIntro>,
+    );
+    expect(screen.getByText('test content')).toBeInTheDocument();
   });
 });
